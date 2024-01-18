ca9116a081d2e213d8a4201a99c52fbfad528759
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-01-18 14:53:39 +0100

    feat(LIB-122): vitest unit test basic config

---
+++ packages/vue-button/src/__tests__/FzButtton.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import FzButton from '../FzButton.vue'
+
+describe('FzButton', () => {
+  it('renders properly', () => {
+    const wrapper = mount(FzButton, { props: { label: 'some text' } })
+    expect(wrapper.text()).toContain('some text')
+  });
+
+  it('emits 1 when clicked', () => {
+    const wrapper = mount(FzButton, { props: { label: 'some text' } });
+    wrapper.trigger('click');
+
+    const clickEvs = wrapper.emitted('click');
+    expect(clickEvs).toHaveLength(1);
+    expect((clickEvs as any[])[0]).toEqual([1]);
+  });
+})
---
+++ vitest.workspace.ts
@@ -0,0 +1,4 @@
+export default [
+    'packages/*/vitest.config.ts',
+    'apps/*/vitest.config.ts',
+]
