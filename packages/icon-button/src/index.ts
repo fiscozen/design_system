25060aacfbd784e210dae28b0efb5dd29f2e3294
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-03-08 09:41:16 +0100

    Update packages/icon-button/src/index.ts
    Co-authored-by: Alen Ajam <alen.ajam4@gmail.com>

--- packages/icon-button/src/index.ts
+++ packages/icon-button/src/index.ts
@@ -1,4 +1,2 @@
-import FzIconButton from './FzIconButton.vue'
-import type { IconButtonVariant } from './types'
-
-export { FzIconButton, IconButtonVariant }
+export { default as FzIconButton } from './FzIconButton.vue';
+export { IconButtonVariant } from './types';
