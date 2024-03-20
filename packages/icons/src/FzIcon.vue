228cb6472a46118fa098d11aaa0199a060bc3d89
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-03-20 12:45:34 +0100

    refactor: automatic formatting

--- packages/icons/src/FzIcon.vue
+++ packages/icons/src/FzIcon.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-8137893ad3/icons';
-import type { IconVariant } from './types';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { byPrefixAndName } from '@awesome.me/kit-8137893ad3/icons'
+import type { IconVariant } from './types'
 
-withDefaults(defineProps<{
-    name: string|string[],
-    size: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
-    variant?: IconVariant,
-}>(),
-{size: 'lg', variant: 'fasl'})
-
+withDefaults(
+  defineProps<{
+    name: string | string[]
+    size: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+    variant?: IconVariant
+  }>(),
+  { size: 'lg', variant: 'fasl' }
+)
 </script>
 
 <template>
-    <font-awesome-icon :icon="typeof name === 'string' ? byPrefixAndName[variant][name] : name" :size="size"/>
+  <font-awesome-icon
+    :icon="typeof name === 'string' ? byPrefixAndName[variant][name] : name"
+    :size="size !== 'md' ? size : undefined"
+  />
 </template>
