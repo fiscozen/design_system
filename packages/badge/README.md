f5d706343a21ac7edda0cc2dcd096d7241cd5c0b
Alen Ajam <alen@fiscozen.it>
2024-03-21 18:34:11 +0100

    feat(LIB-274): init badge component (#13)
    * feat(LIB-274): init badge component
    
    * fix package description
    
    Co-authored-by: Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
    
    * refactor(LIB-274): declare map object outside of computed
    
    * refactor(LIB-271): remove text white from warning variant
    
    * test(LIB-274): update snapshots
    
    * refactor(LIB-274): remove satisfies
    
    ---------
    
    Co-authored-by: Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>

---
+++ packages/badge/README.md
@@ -0,0 +1,3 @@
+# @fiscozen/badge
+
+Badges are used to inform the user of the status of specific data.
---
+++ packages/badge/src/FzBadge.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    /**
+     * Color variant
+     */
+    color: "black" | "error" | "warning" | "success" | "info";
+  }>(),
+  {
+    color: "black",
+  },
+);
+
+const mapColorToClasses = {
+  black: "bg-core-black text-core-white",
+  error: "bg-semantic-error text-core-white",
+  warning: "bg-semantic-warning",
+  success: "bg-semantic-success text-core-white",
+  info: "bg-semantic-info text-core-white",
+};
+
+const classes = computed(() => [
+  "text-xs px-12 rounded-xl w-fit",
+  mapColorToClasses[props.color],
+]);
+</script>
