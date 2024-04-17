92314ea899ddf89f9d6e0e8c5ba4888f6c9c0cee
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-04-17 15:30:31 +0200

    LIB-288 refactor(navlink): remove unnecessary iconOnly prop

--- apps/storybook/src/stories/Navbar.stories.ts
+++ apps/storybook/src/stories/Navbar.stories.ts
@@ -27,7 +27,7 @@ const horizontalNavbar = (args) => ({
   template: `
     <FzNavbar>
       <template #brand-logo="{isMobile}">
-        <FzNavlink :iconOnly="true" iconName="fiscozen" iconVariant="fak" iconSize="xl" :class="{'!text-[20px]': !isMobile, '!text-[24px]': isMobile}" /> 
+        <FzNavlink iconName="fiscozen" iconVariant="fak" iconSize="xl" :class="{'!text-[20px]': !isMobile, '!text-[24px]': isMobile}" /> 
       </template>
 
       <template #navigation>
@@ -61,18 +61,18 @@ const verticalNavbar = (args) => ({
     <div class="h-screen m-0">
       <FzNavbar variant="vertical">
         <template #brand-logo="{isMobile}">
-          <FzNavlink :iconOnly="true" iconName="fiscozen" iconVariant="fak" iconSize="xl" :class="{'!text-[20px]': !isMobile, '!text-[24px]': isMobile}" /> 
+          <FzNavlink iconName="fiscozen" iconVariant="fak" iconSize="xl" :class="{'!text-[20px]': !isMobile, '!text-[24px]': isMobile}" /> 
         </template>
 
         <template #navigation>
-          <FzNavlink :iconOnly="true" iconName="suitcase" /> 
-          <FzNavlink :iconOnly="true" iconName="folder-user" /> 
-          <FzNavlink :iconOnly="true" iconName="credit-card" /> 
-          <FzNavlink :iconOnly="true" iconName="cart-shopping" /> 
-          <FzNavlink :iconOnly="true" iconName="calendar-lines" /> 
-          <FzNavlink :iconOnly="true" iconName="file-check" /> 
-          <FzNavlink :iconOnly="true" iconName="gear" /> 
-          <FzNavlink :iconOnly="true" iconName="screwdriver-wrench" /> 
+          <FzNavlink iconName="suitcase" /> 
+          <FzNavlink iconName="folder-user" /> 
+          <FzNavlink iconName="credit-card" /> 
+          <FzNavlink iconName="cart-shopping" /> 
+          <FzNavlink iconName="calendar-lines" /> 
+          <FzNavlink iconName="file-check" /> 
+          <FzNavlink iconName="gear" /> 
+          <FzNavlink iconName="screwdriver-wrench" /> 
         </template>
         
         <template #notifications="{isHorizontal, isVertical, isMobile}">
