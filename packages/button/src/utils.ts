0976a1f51ddbb7d59c441e5d4ad56b7a40835861
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-05-15 13:30:14 +0200

    LIB-429 feat(button): update design and iconbutton refactor

---
+++ packages/button/src/utils.ts
@@ -0,0 +1,11 @@
+import { ButtonSize } from "./types"
+import { IconSize } from "@fiscozen/icons"
+
+export const iconSizeMap: {
+  [key in ButtonSize]: IconSize
+} = {
+  xs: 'sm',
+  sm: 'md',
+  md: 'lg',
+  lg: 'lg'
+}
--- packages/icons/src/types.ts
+++ packages/icons/src/types.ts
@@ -1,4 +1,4 @@
 type IconVariant = 'fas' | 'far' | 'fal' | 'fat' | 'fad' | 'fass' | 'fasr' | 'fasl' | 'fast' | 'fak'
-type IconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+type IconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
 
 export type { IconVariant, IconSize }
