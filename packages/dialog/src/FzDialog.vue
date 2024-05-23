c3ee97825c6a354a4e91f6029e389ccc0578a49e
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-05-23 14:55:27 +0200

    LIB-446 chore(dialog): format

--- packages/dialog/src/FzDialog.vue
+++ packages/dialog/src/FzDialog.vue
@@ -1,30 +1,27 @@
 <template>
-    <dialog ref="dialog" @close="visible = false" :class="dialogClasses">
-        <div :class="[staticClasses, classes]">
-            <div class="flex items-center p-12 w-full border-b-1 border-grey-100">
-                <slot name="header"></slot>
-            </div>
-            <div class="grow">
-                <slot name="body"></slot>
-            </div>
-            <div class="flex flex-row p-12 border-t-1 border-grey-100 items-center">
-                <slot name="footer"></slot>
-            </div>
-        </div>
-    </dialog>
+  <dialog ref="dialog" @close="visible = false" :class="dialogClasses">
+    <div :class="[staticClasses, classes]">
+      <div class="flex items-center p-12 w-full border-b-1 border-grey-100">
+        <slot name="header"></slot>
+      </div>
+      <div class="grow">
+        <slot name="body"></slot>
+      </div>
+      <div class="flex flex-row p-12 border-t-1 border-grey-100 items-center">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </dialog>
 </template>
 
 <script setup lang="ts">
-import {computed, ref, onMounted, onUnmounted} from 'vue'
-import {FzDialogProps} from './types'
+import { computed, ref, onMounted, onUnmounted } from "vue";
+import { FzDialogProps } from "./types";
 
-const props = withDefaults(
-    defineProps<FzDialogProps>(),
-    {
-        size: 'md'
-    }
-)
-const emit = defineEmits(['confirm', 'cancel']);
+const props = withDefaults(defineProps<FzDialogProps>(), {
+  size: "md",
+});
+const emit = defineEmits(["confirm", "cancel"]);
 
 const dialog = ref<HTMLDialogElement>();
 const visible = ref(false);
@@ -41,129 +38,133 @@ defineExpose({
 });
 
 const handleBackdropClick = (event: MouseEvent) => {
-        if (!dialog.value) {
-            return
-        }
-        var rect = dialog.value.getBoundingClientRect();
-        var isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
-            rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            dialog.value.close();
-        }
-}
+  if (!dialog.value) {
+    return;
+  }
+  var rect = dialog.value.getBoundingClientRect();
+  var isInDialog =
+    rect.top <= event.clientY &&
+    event.clientY <= rect.top + rect.height &&
+    rect.left <= event.clientX &&
+    event.clientX <= rect.left + rect.width;
+  if (!isInDialog) {
+    dialog.value.close();
+  }
+};
 onMounted(() => {
-    dialog.value?.addEventListener('click', handleBackdropClick);
-})
+  dialog.value?.addEventListener("click", handleBackdropClick);
+});
 onUnmounted(() => {
-    dialog.value?.removeEventListener('click', handleBackdropClick);
-})
+  dialog.value?.removeEventListener("click", handleBackdropClick);
+});
 
 const staticClasses = [
-    'flex',
-    'flex-col',
-    'border-1',
-    'rounded',
-    'bg-core-white'
-]
+  "flex",
+  "flex-col",
+  "border-1",
+  "rounded",
+  "bg-core-white",
+];
 
 const dialogClasses = computed(() => {
-    let res: Record<string, boolean> = {};
-    if (props.isDrawer) {
-        res = {
-            'm-0': true,
-            'fixed': true,
-            'top-0': true,
-            'ml-auto': true,
-            'max-h-screen': true,
-        }
-        return res
-    }
-    switch (props.size) {
-        case 'sm':
-            res = {} 
-            break;
-        case 'md':
-            res = {
-                'xs:max-sm:m-0': true,
-                'xs:max-sm:max-h-screen': true,
-                'xs:max-sm:h-screen': true,
-                'xs:max-sm:w-screen': true,
-                'xs:max-sm:max-w-screen-xl': true,
-            } 
-         case 'lg':
-            res = {
-                'xs:max-md:m-0': true,
-                'xs:max-md:max-h-screen': true,
-                'xs:max-md:h-screen': true,
-                'xs:max-md:w-screen': true,
-                'xs:max-md:max-w-screen-xl': true,
-            } 
-          case 'xl':
-            res = {
-                'xs:max-xl:m-0': true,
-                'xs:max-xl:max-h-screen': true,
-                'xs:max-xl:h-screen': true,
-                'xs:max-xl:w-screen': true,
-                'xs:max-xl:max-w-screen-xl': true,
-            } 
-            break;   break; 
-        default:
-            res = {}
-            break;
-    }
-    return res
-})
+  let res: Record<string, boolean> = {};
+  if (props.isDrawer) {
+    res = {
+      "m-0": true,
+      fixed: true,
+      "top-0": true,
+      "ml-auto": true,
+      "max-h-screen": true,
+    };
+    return res;
+  }
+  switch (props.size) {
+    case "sm":
+      res = {};
+      break;
+    case "md":
+      res = {
+        "xs:max-sm:m-0": true,
+        "xs:max-sm:max-h-screen": true,
+        "xs:max-sm:h-screen": true,
+        "xs:max-sm:w-screen": true,
+        "xs:max-sm:max-w-screen-xl": true,
+      };
+    case "lg":
+      res = {
+        "xs:max-md:m-0": true,
+        "xs:max-md:max-h-screen": true,
+        "xs:max-md:h-screen": true,
+        "xs:max-md:w-screen": true,
+        "xs:max-md:max-w-screen-xl": true,
+      };
+    case "xl":
+      res = {
+        "xs:max-xl:m-0": true,
+        "xs:max-xl:max-h-screen": true,
+        "xs:max-xl:h-screen": true,
+        "xs:max-xl:w-screen": true,
+        "xs:max-xl:max-w-screen-xl": true,
+      };
+      break;
+      break;
+    default:
+      res = {};
+      break;
+  }
+  return res;
+});
 
 const classes = computed(() => {
-    let res: Record<string, boolean> = {};
-    if (props.isDrawer) {
-        res['w-[480px]'] = true
-        res['h-screen'] = true
+  let res: Record<string, boolean> = {};
+  if (props.isDrawer) {
+    res["w-[480px]"] = true;
+    res["h-screen"] = true;
 
-        return res;
-    }
-    switch (props.size) {
-        case 'sm':
-            res = {
-                'w-[320px]': true,
-                'min-h-[200px]': true,
-                'max-h-[432px]': true
-            } 
-            break;
-        case 'md':
-            res = {
-                'w-screen sm:w-[480px]': true,
-                'min-h-[300px]': true,
-                'sm:max-h-[600px]': true,
-                'h-screen sm:h-auto': true
-            } 
-            break;
-        case 'lg':
-            res = {
-                'w-screen md:w-[640px]': true,
-                'min-h-[300px]': true,
-                'md:max-h-[600px]': true,
-                'h-screen md:h-auto': true
-            } 
-            break;
-        case 'xl':
-            res = {
-                'w-screen xl:w-[960px]': true,
-                'min-h-[400px]': true,
-                'xl:max-h-[600px]': true,
-                'h-screen xl:h-auto': true
-            } 
-            break;
-        default:
-            break;
-    }
     return res;
-})
+  }
+  switch (props.size) {
+    case "sm":
+      res = {
+        "w-[320px]": true,
+        "min-h-[200px]": true,
+        "max-h-[432px]": true,
+      };
+      break;
+    case "md":
+      res = {
+        "w-screen sm:w-[480px]": true,
+        "min-h-[300px]": true,
+        "sm:max-h-[600px]": true,
+        "h-screen sm:h-auto": true,
+      };
+      break;
+    case "lg":
+      res = {
+        "w-screen md:w-[640px]": true,
+        "min-h-[300px]": true,
+        "md:max-h-[600px]": true,
+        "h-screen md:h-auto": true,
+      };
+      break;
+    case "xl":
+      res = {
+        "w-screen xl:w-[960px]": true,
+        "min-h-[400px]": true,
+        "xl:max-h-[600px]": true,
+        "h-screen xl:h-auto": true,
+      };
+      break;
+    default:
+      break;
+  }
+  return res;
+});
 </script>
 
 <style>
 dialog::backdrop {
-    background: var(--core-black, #2C282F);
-    opacity: 0.8;
+  background: var(--core-black, #2c282f);
+  opacity: 0.8;
 }
 </style>
--- packages/dialog/src/__test__/FzDialog.test.ts
+++ packages/dialog/src/__test__/FzDialog.test.ts
@@ -1,67 +1,66 @@
 import { mount } from "@vue/test-utils";
-import figmaTokens from "@fiscozen/style/tokens.json"
+import figmaTokens from "@fiscozen/style/tokens.json";
 import { describe, it, expect } from "vitest";
-import { FzSimpleDialog } from '../'
+import { FzSimpleDialog } from "../";
 
-const viewports: Record<string, number> = Object.entries(figmaTokens.global.breakpoint)
-  .reduce((acc: Record<string, number>, curr: [string, any]) => {
-    acc[curr[0]] = Number(curr[1].value.slice(0, -2))
-    return acc;
-  }, {})
+const viewports: Record<string, number> = Object.entries(
+  figmaTokens.global.breakpoint,
+).reduce((acc: Record<string, number>, curr: [string, any]) => {
+  acc[curr[0]] = Number(curr[1].value.slice(0, -2));
+  return acc;
+}, {});
 
 describe("FzDialog", () => {
-  it('should match snapshot - md', () => {
-    global.innerWidth = viewports['lg']
+  it("should match snapshot - md", () => {
+    global.innerWidth = viewports["lg"];
+    const wrapper = mount(FzSimpleDialog, {
+      props: {},
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should match snapshot - md - xs screen", () => {
+    global.innerWidth = viewports["xs"];
+    const wrapper = mount(FzSimpleDialog, {
+      props: {},
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should match snapshot - sm", () => {
     const wrapper = mount(FzSimpleDialog, {
       props: {
-      }
-    })
+        size: "sm",
+      },
+    });
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-  it('should match snapshot - md - xs screen', () => {
-    global.innerWidth = viewports['xs']
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should match snapshot - lg", () => {
     const wrapper = mount(FzSimpleDialog, {
       props: {
-      }
-    })
+        size: "lg",
+      },
+    });
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-  it('should match snapshot - sm', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should match snapshot - xl", () => {
     const wrapper = mount(FzSimpleDialog, {
       props: {
-        size: 'sm'
-      }
-    })
+        size: "xl",
+      },
+    });
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-  it('should match snapshot - lg', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it("should match snapshot - drawer", () => {
     const wrapper = mount(FzSimpleDialog, {
       props: {
-        size: 'lg'
-      }
-    })
+        isDrawer: true,
+      },
+    });
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-  it('should match snapshot - xl', () => {
-    const wrapper = mount(FzSimpleDialog, {
-      props: {
-        size: 'xl'
-      }
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-  it('should match snapshot - drawer', () => {
-    const wrapper = mount(FzSimpleDialog, {
-      props: {
-        isDrawer: true
-      }
-    })
-
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 });
