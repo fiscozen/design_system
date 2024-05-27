8a1f23f3d827827f7a69cb0b65ac09d6a29b0383
Cristian Barraco <cristian.barraco96@gmail.com>
2024-05-27 17:05:17 +0200

    feat(LIB-436): implemented radio and radio group components

---
+++ apps/storybook/src/stories/Radio.stories.ts
@@ -0,0 +1,123 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { FzRadio } from '@fiscozen/radio'
+import { FzIcon } from '@fiscozen/icons'
+
+
+const meta = {
+    title: 'Radio',
+    component: FzRadio,
+    tags: ['autodocs'],
+    argTypes: {
+        size: {
+            options: ['sm', 'md'],
+            control: {
+                type: 'select'
+            }
+        }
+    }
+} satisfies Meta<typeof FzRadio>
+
+export default meta
+
+type RadioStory = StoryObj<typeof FzRadio>
+
+const Template: RadioStory = {
+    render: (args) => ({
+        components: { FzRadio, FzIcon },
+        setup() {
+            return {
+                args,
+            }
+        },
+        template: `<FzRadio v-bind="args"  />`
+    }),
+    args: {
+        size: 'sm',
+        label: 'Radio'
+    },
+}
+
+export const Medium: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio'
+    }
+}
+
+export const Small: RadioStory = {
+    ...Template,
+    args: {
+        size: 'sm',
+        label: 'Radio'
+    }
+}
+
+export const CheckedDefault: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        checked: true
+    }
+}
+
+export const Disabled: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        disabled: true
+    }
+}
+
+export const CheckedDisabled: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        checked: true,
+        disabled: true
+    }
+}
+
+export const Error: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        error: true,
+        errorText: 'Error message'
+    }
+}
+
+export const Emphasis: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        emphasis: true
+    }
+}
+
+export const Standalone: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Radio',
+        standalone: true
+    }
+}
+
+export const VeryLongLabel: RadioStory = {
+    ...Template,
+    args: {
+        size: 'md',
+        label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+    },
+    parameters: {
+        viewport: {
+            defaultViewport: 'xs'
+        }
+    }
+}
