6d15f182a958286f1d96caeed8ab819191911c92
Cristian Barraco <cristian.barraco96@gmail.com>
2024-05-31 16:55:42 +0200

    fix(LIB-436): pr comments

--- packages/radio/src/FzRadioGroup.vue
+++ packages/radio/src/FzRadioGroup.vue
@@ -1,12 +1,15 @@
 <template>
-  <div :class="computedContainerClass">
-    <label :for="name" :class="computedLabelClass">
+  <div :class="[staticContainerClass, computedContainerClass]">
+    <label :for="name" :class="[staticLabelClass, computedLabelClass]">
       <span>{{ label }}<span v-if="required"> *</span></span>
       <p :class="computedHelpTextClass" v-if="$slots.help">
         <slot name="help" />
       </p>
     </label>
-    <div :class="computedSlotContainerClass" test-id="slot-container">
+    <div
+      :class="[staticSlotContainerClass, computedSlotContainerClass]"
+      test-id="slot-container"
+    >
       <slot :radioGroupProps="controlledProps" />
     </div>
     <FzRadioErrorText :size="size" v-if="error && $slots.error">
@@ -34,27 +37,32 @@ const controlledProps = computed(() => ({
   name: props.name,
 }));
 
+const staticLabelClass = "flex flex-col";
+const staticContainerClass = "flex flex-col";
+const staticSlotContainerClass = "flex flex-col";
+
 const computedHelpTextClass = computed(() => [
-  props.size === "sm" ? "text-xs" : "text-sm",
+  props.size === "sm" ? "text-xs" : "",
+  props.size === "md" ? "text-sm" : "",
   props.disabled ? "text-grey-400" : "text-grey-500",
 ]);
 
 const computedLabelClass = computed(() => [
-  "flex flex-col",
   mapSizeToClasses[props.size],
-  props.size === "sm" ? "gap-4" : "gap-6",
+  props.size === "sm" ? "gap-4" : "",
+  props.size === "md" ? "gap-6" : "",
   props.disabled ? "text-grey-400" : "text-core-black",
 ]);
 
 const computedContainerClass = computed(() => [
-  "flex flex-col",
   mapSizeToClasses[props.size],
-  props.size === "sm" ? "gap-10" : "gap-12",
+  props.size === "sm" ? "gap-10" : "",
+  props.size === "md" ? "gap-12" : "",
 ]);
 
 const computedSlotContainerClass = computed(() => [
-  "flex flex-col",
   mapSizeToClasses[props.size],
-  props.size === "sm" ? "gap-6" : "gap-8",
+  props.size === "sm" ? "gap-6" : "",
+  props.size === "md" ? "gap-8" : "",
 ]);
 </script>
