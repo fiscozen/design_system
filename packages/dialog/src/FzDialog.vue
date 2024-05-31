df0c9c0453a233912accdc3ef266b462bd9a9112
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-05-31 09:55:23 +0200

    LIB-446 refactor(dialog): PR related changes

--- packages/dialog/src/FzDialog.vue
+++ packages/dialog/src/FzDialog.vue
@@ -71,93 +71,90 @@ const dialogStaticClasses = {
 }
 
 const dialogClasses = computed(() => {
-  let res: Record<string, boolean> = {};
+  let res: string[] = [];
   if (props.isDrawer) {
-    res = {
-      "m-0": true,
-      fixed: true,
-      "top-0": true,
-      "ml-auto": true,
-      "max-h-screen": true,
-    };
+    res = [
+      "m-0",
+      "fixed",
+      "top-0",
+      "ml-auto",
+      "max-h-screen",
+    ];
     return res;
   }
   switch (props.size) {
     case "sm":
-      res = {};
+      res = [];
       break;
     case "md":
-      res = {
-        "xs:max-sm:m-0": true,
-        "xs:max-sm:max-h-screen": true,
-        "xs:max-sm:h-screen": true,
-        "xs:max-sm:w-screen": true,
-        "xs:max-sm:max-w-screen-xl": true,
-      };
+      res = [
+        "xs:max-sm:m-0",
+        "xs:max-sm:max-h-screen",
+        "xs:max-sm:h-screen",
+        "xs:max-sm:w-screen",
+        "xs:max-sm:max-w-screen-xl",
+      ];
     case "lg":
-      res = {
-        "xs:max-md:m-0": true,
-        "xs:max-md:max-h-screen": true,
-        "xs:max-md:h-screen": true,
-        "xs:max-md:w-screen": true,
-        "xs:max-md:max-w-screen-xl": true,
-      };
+      res = [
+        "xs:max-md:m-0",
+        "xs:max-md:max-h-screen",
+        "xs:max-md:h-screen",
+        "xs:max-md:w-screen",
+        "xs:max-md:max-w-screen-xl",
+      ];
     case "xl":
-      res = {
-        "xs:max-xl:m-0": true,
-        "xs:max-xl:max-h-screen": true,
-        "xs:max-xl:h-screen": true,
-        "xs:max-xl:w-screen": true,
-        "xs:max-xl:max-w-screen-xl": true,
-      };
-      break;
+      res = [
+        "xs:max-xl:m-0",
+        "xs:max-xl:max-h-screen",
+        "xs:max-xl:h-screen",
+        "xs:max-xl:w-screen",
+        "xs:max-xl:max-w-screen-xl",
+      ];
       break;
     default:
-      res = {};
+      res = [];
       break;
   }
   return res;
 });
 
 const classes = computed(() => {
-  let res: Record<string, boolean> = {};
+  let res: string[] = [];
   if (props.isDrawer) {
-    res["w-[480px]"] = true;
-    res["h-screen"] = true;
-
+    res = ["w-[480px]", "h-screen"];
     return res;
   }
   switch (props.size) {
     case "sm":
-      res = {
-        "w-[320px]": true,
-        "min-h-[200px]": true,
-        "max-h-[432px]": true,
-      };
+      res = [
+        "w-[320px]",
+        "min-h-[200px]",
+        "max-h-[432px]",
+      ];
       break;
     case "md":
-      res = {
-        "w-screen sm:w-[480px]": true,
-        "min-h-[300px]": true,
-        "sm:max-h-[600px]": true,
-        "h-screen sm:h-auto": true,
-      };
+      res = [
+        "w-screen sm:w-[480px]",
+        "min-h-[300px]",
+        "sm:max-h-[600px]",
+        "h-screen sm:h-auto",
+      ];
       break;
     case "lg":
-      res = {
-        "w-screen md:w-[640px]": true,
-        "min-h-[300px]": true,
-        "md:max-h-[600px]": true,
-        "h-screen md:h-auto": true,
-      };
+      res = [
+        "w-screen md:w-[640px]",
+        "min-h-[300px]",
+        "md:max-h-[600px]",
+        "h-screen md:h-auto",
+      ];
       break;
     case "xl":
-      res = {
-        "w-screen xl:w-[960px]": true,
-        "min-h-[400px]": true,
-        "xl:max-h-[600px]": true,
-        "h-screen xl:h-auto": true,
-      };
+      res = [
+        "w-screen xl:w-[960px]",
+        "min-h-[400px]",
+        "xl:max-h-[600px]",
+        "h-screen xl:h-auto",
+      ];
       break;
     default:
       break;
