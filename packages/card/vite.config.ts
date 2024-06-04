a2285e045aee3d222484e191f5beb91847464caa
Cristian Barraco <cristian.barraco96@gmail.com>
2024-06-04 10:22:46 +0200

    feat(LIB-468): card implementation

---
+++ packages/card/vite.config.ts
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'FzCard',
+    },
+    rollupOptions: {
+      external: ['vue', "@fiscozen/icons", "@fiscozen/button"],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    }
+  }
+})
