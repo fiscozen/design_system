b09e29f4ee026b95fe0da793b8a4d5363ef4199d
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-06-06 11:10:16 +0200

    chore(tooltip): props have defaults, can be optional

--- packages/tooltip/src/types.ts
+++ packages/tooltip/src/types.ts
@@ -3,8 +3,8 @@ import { FzFloatingPosition } from '@fiscozen/composables'
 export type FzTooltipStatus = 'neutral' | 'informative' | 'positive' | 'alert' | 'error'
 
 export interface FzTooltipProps {
-  status: FzTooltipStatus
-  position: FzFloatingPosition
+  status?: FzTooltipStatus
+  position?: FzFloatingPosition
   text?: string
   withIcon?: boolean
 }
