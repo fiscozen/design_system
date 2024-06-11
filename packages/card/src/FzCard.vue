0e3eed0bc5e6bee06954ad639a2af66c95c6e78d
Cristian Barraco <cristian.barraco96@gmail.com>
2024-06-11 11:16:34 +0200

    fix(LIB-468):update design

--- packages/card/src/FzCard.vue
+++ packages/card/src/FzCard.vue
@@ -1,58 +1,81 @@
 <template>
   <section :class="[sectionStaticClass, backgroundColor]">
-    <header :class="[headerStaticClass, borderColor]">
-      <h2
-        class="text-core-black font-medium text-2xl m-0 p-0 break-words"
-        :title="title"
-      >
-        {{ title }}
-      </h2>
+    <header :class="[headerContainerComputedClass, borderColor]">
+      <div :class="headerStaticClass">
+        <div class="flex flex-row gap-12 items-center">
+          <h2
+            v-if="title"
+            class="text-core-black font-medium text-2xl m-0 p-0 break-words"
+            :title="title"
+          >
+            {{ title }}
+          </h2>
+          <slot name="header"></slot>
+        </div>
+        <FzIconButton
+          v-if="collapsible"
+          :iconName="isOpen ? 'chevron-up' : 'chevron-down'"
+          @click="isOpen = !isOpen"
+          variant="invisible"
+        />
+      </div>
+      <slot name="header-content"></slot>
     </header>
-    <article :class="['p-20 flex-1', contentClass]">
-      <slot></slot>
-    </article>
-    <footer v-if="atLeastOneButton" :class="[footerStaticClass, borderColor]">
-      <FzIconButton
-        v-if="tertiaryAction"
-        @click="$emit('click:tertiary')"
-        :iconName="tertiaryAction.icon"
-        variant="invisible"
-      />
-      <FzButton
-        v-if="secondaryAction"
-        @click="$emit('click:secondary')"
-        :label="secondaryAction.label"
-        variant="secondary"
-      />
-      <FzButton
-        v-if="primaryAction"
-        @click="$emit('click:primary')"
-        :label="primaryAction.label"
-        variant="primary"
-      />
-    </footer>
+    <template v-if="showContent">
+      <article :class="['p-20', contentClass]">
+        <slot></slot>
+      </article>
+      <footer v-if="atLeastOneButton" :class="[footerStaticClass, borderColor]">
+        <FzIconButton
+          v-if="tertiaryAction"
+          @click="$emit('click:tertiary')"
+          :iconName="tertiaryAction.icon"
+          variant="invisible"
+        />
+        <FzButton
+          v-if="secondaryAction"
+          @click="$emit('click:secondary')"
+          :label="secondaryAction.label"
+          variant="secondary"
+        />
+        <FzButton
+          v-if="primaryAction"
+          @click="$emit('click:primary')"
+          :label="primaryAction.label"
+          variant="primary"
+        />
+      </footer>
+    </template>
   </section>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
-import { FzCardColor, FzCardProps } from "./types";
+import { computed, onMounted, ref } from "vue";
+import { FzCardProps } from "./types";
 import { FzButton, FzIconButton } from "@fiscozen/button";
 
 const props = defineProps<FzCardProps>();
+const isOpen = ref(props.defaultExpanded ?? false);
+
 const sectionStaticClass =
   "border-1 border-solid border-grey-100 rounded flex flex-col";
-const headerStaticClass = "h-72 border-b-1 border-solid p-20";
+const headerStaticClass =
+  "h-64 border-solid p-16 flex flex-row justify-between";
 const footerStaticClass =
-  "h-64 border-t-1 border-solid p-20 flex justify-end gap-8 items-center";
+  "h-64 border-t-1 border-solid p-16 flex justify-end gap-8 items-center";
+
+const showContent = computed(() => isOpen.value || !props.collapsible);
+const headerContainerComputedClass = computed(() => [
+  showContent.value ? "border-b-1" : "border-b-0",
+]);
 
 const backgroundColor = computed(() => {
   switch (props.color) {
-    case FzCardColor.Blue:
+    case "blue":
       return "bg-background-alice-blue";
-    case FzCardColor.Orange:
+    case "orange":
       return "bg-background-seashell";
-    case FzCardColor.Purple:
+    case "purple":
       return "bg-background-pale-purple";
     default:
       return "bg-core-white";
@@ -61,11 +84,11 @@ const backgroundColor = computed(() => {
 
 const borderColor = computed(() => {
   switch (props.color) {
-    case FzCardColor.Blue:
+    case "blue":
       return "border-blue-200";
-    case FzCardColor.Orange:
+    case "orange":
       return "border-orange-200";
-    case FzCardColor.Purple:
+    case "purple":
       return "border-purple-200";
     default:
       return "border-grey-100";
