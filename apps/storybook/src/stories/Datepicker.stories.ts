fff36798df3155f398aac7fc51ab8690d9b10bd0
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-07-16 12:23:00 +0200

    LIB-555 feat(datepicker): implement design variants

--- apps/storybook/src/stories/Datepicker.stories.ts
+++ apps/storybook/src/stories/Datepicker.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import { FzDatepicker } from '@fiscozen/datepicker'
+import {ref} from 'vue'
 
 const meta: Meta<typeof FzDatepicker> = {
   title: '@fiscozen/datepicker/FzDatepicker',
@@ -12,19 +13,126 @@ const meta: Meta<typeof FzDatepicker> = {
 
 type Story = StoryObj<typeof meta>
 
+const Template: Story = {
+  render: (args) => ({
+    components: {FzDatepicker},
+    setup() {
+      const date = ref();
+      return {
+        date,
+        args
+      }
+    },
+    template: `<FzDatepicker v-bind="args" v-model="date" />`
+  }),
+}
+
 const Default: Story = {
-  args: {}
+  ...Template,
+  args: {},
 }
 
 const Range: Story = {
+  ...Template,
   args: {
     range: true
   }
 }
 
+const AutoRange: Story = {
+  ...Template,
+  args: {
+    range: { autoRange: 5},
+  }
+}
+
+const WeekPicker: Story = {
+  ...Template,
+  args: {
+    weekPicker: true,
+  }
+}
+
+const MultiCalendar: Story = {
+  ...Template,
+  args: {
+    range: true,
+    multiCalendars: true
+  }
+}
+
+const MonthPicker: Story = {
+  ...Template,
+  args: {
+    monthPicker: true
+  }
+}
+
+const YearPicker: Story = {
+  ...Template,
+  args: {
+    yearPicker: true
+  }
+}
+
+const MultiDates: Story = {
+  ...Template,
+  args: {
+    multiDates: {limit: 4},
+    autoApply: false
+  }
+}
+
+const today = new Date()
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1);
+const afterTomorrow = new Date(tomorrow)
+afterTomorrow.setDate(tomorrow.getDate() + 1);
+
+const DisabledDates: Story = {
+  ...Template,
+  args: {
+    disabledDates: [tomorrow, afterTomorrow]
+  }
+}
+
+const isDisabled = (date: Date) => {
+  let res = false;
+
+  if (date.getFullYear() === 2022) {
+    res = true;
+  }
+
+  return res;
+}
+const ComplexDisabledDates: Story = {
+  ...Template,
+  args: {
+    disabledDates: isDisabled
+  }
+}
+
+const InlineTimePicker: Story = {
+  ...Template,
+  args: {
+    timePickerInline: true,
+    enableTimePicker: true,
+    enableMinutes: true,
+    is24: true
+  }
+}
 export {
   Default,
-  Range
+  Range,
+  AutoRange,
+  WeekPicker,
+  MultiCalendar,
+  MonthPicker,
+  YearPicker,
+  MultiDates,
+  DisabledDates,
+  ComplexDisabledDates,
+  InlineTimePicker
 }
 
 export default meta
