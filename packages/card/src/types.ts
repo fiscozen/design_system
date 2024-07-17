fde97a5345f22c47478eccd7811e2b0d3dbddac3
Cristian Barraco <cristian.barraco96@gmail.com>
2024-07-17 13:03:47 +0200

    fix(LIB-523): comments pr

--- packages/card/src/types.ts
+++ packages/card/src/types.ts
@@ -1,12 +1,39 @@
 export type FzCardProps = {
+  /**
+   * The title of the card
+   */
   title: string;
+  /**
+   * The background color of the card
+   */
   color?: FzCardColor;
+  /**
+   * The primary action button
+   */
   primaryAction?: FzCardButton;
+  /**
+   * The secondary action button
+   */
   secondaryAction?: FzCardButton;
+  /**
+   * The tertiary action button
+   */
   tertiaryAction?: FzCardIconButton;
+  /**
+   * Custom css class to apply to the content
+   */
   contentClass?: string;
+  /**
+   * Whether the card is collapsible
+   */
   collapsible?: boolean;
+  /**
+   * Whether the card is expanded by default (only if collapsible)
+   */
   defaultExpanded?: boolean;
+  /**
+   * Whether the card content is always alive (never destroyed) when collapsed
+   */
   alwaysAlive?: boolean;
 };
 
@@ -19,3 +46,36 @@ type FzCardIconButton = {
 };
 
 export type FzCardColor = "purple" | "orange" | "blue";
+
+export interface FzCardEvents {
+  /**
+   * Event emitted when the primary action is clicked
+   * @type {() => void}
+   */
+  (event: 'fzprimary:click'): void
+  /**
+   * Event emitted when the secondary action is clicked
+   * @type {() => void}
+   */
+  (event:'fzsecondary:click'): void,
+  /**
+   * Event emitted when the tertiary action is clicked
+   * @type {() => void}
+   */
+  (event:'fztertiary:click'): void,
+}
+
+export interface FzCardSlots {
+  /**
+   * Slot for the content of the card
+   */
+  default(props:{}): any;
+  /**
+   * Slot for the header, it will be displayed on the left of the title
+   */
+  header(props:{}): any;
+  /**
+   * Slot for the header content, it will be displayed below the title
+   */
+  'header-content'(props:{}): any;
+}
