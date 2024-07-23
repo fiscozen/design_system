e0d7d541753fd0d959e01069a374723c50351ea0
Cristian Barraco <cristian.barraco96@gmail.com>
2024-07-23 15:46:13 +0200

    feat(LIB-541): first implementation

--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     ...tailwindConfig,
     content: [
         "./src/**/*.{html,js,ts,vue}", 
-        "./node_modules/@fiscozen/**/src/**/*.{html,js,ts,vue}"
+        "./node_modules/@fiscozen/**/src/**/*.{html,js,ts,vue}",
+        "./node_modules/@fiscozen/input/src/**/*.{html,js,ts,vue}",
     ],
 }
---
+++ packages/input/src/__tests__/FzInput.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { FzInput } from '..'
+
+describe.concurrent('FzInput', () => {
+  it('matches snaphost', async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {},
+      slots: {}
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
