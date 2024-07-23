d1ddb864cecf4463a210bb010c70ca957bfb5f4f
Cristian Barraco <cristian.barraco96@gmail.com>
2024-07-23 18:07:38 +0200

    feat(LIB-541) input component

--- packages/input/src/__tests__/FzInput.spec.ts
+++ packages/input/src/__tests__/FzInput.spec.ts
@@ -5,10 +5,139 @@ import { FzInput } from "..";
 describe.concurrent("FzInput", () => {
   it("matches snaphost", async ({ expect }) => {
     const wrapper = mount(FzInput, {
-      props: {},
+      props: {
+        label: "Label",
+      },
       slots: {},
     });
 
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("renders label", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.text()).toContain("Label");
+  });
+
+  it("renders leftIcon", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        leftIcon: "calendar-lines",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find(".fa-calendar-lines")).toBeTruthy();
+  });
+
+  it("renders rightIcon", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        rightIcon: "credit-card",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find(".fa-credit-card")).toBeTruthy();
+  });
+
+  it("renders helpText", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+      },
+      slots: {
+        helpText: "This is a helper text",
+      },
+    });
+
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("This is a helper text");
+  });
+
+  it("renders errorMessage", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        error: true,
+      },
+      slots: {
+        errorMessage: "This is an error message",
+      },
+    });
+
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("This is an error message");
+  });
+
+  it("renders disabled", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        disabled: true,
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find("input").attributes("disabled")).toBe("");
+  });
+
+  it("renders required", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        required: true,
+      },
+      slots: {},
+    });
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find("input").attributes("required")).toBe("");
+    expect(wrapper.text()).toContain("*");
+  });
+
+  it("renders email type", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        type: "email",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find("input").attributes("type")).toBe("email");
+  });
+
+  it("renders tel type", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        type: "tel",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find("input").attributes("type")).toBe("tel");
+  });
+
+  it("renders password type", async ({ expect }) => {
+    const wrapper = mount(FzInput, {
+      props: {
+        label: "Label",
+        type: "password",
+      },
+      slots: {},
+    });
+
+    expect(wrapper.find("input").attributes("type")).toBe("password");
   });
 });
--- packages/input/src/useInputStyle.ts
+++ packages/input/src/useInputStyle.ts
@@ -1,60 +1,64 @@
 import { computed, Ref } from "vue";
 import { FzInputProps } from "./types";
 
-export default function useInputStyle(props: FzInputProps, container: Ref<HTMLElement | null>) {
-    const containerWidth = computed(() => container.value ? `${container.value.clientWidth}px` : 'auto');
-    
+export default function useInputStyle(
+  props: FzInputProps,
+  container: Ref<HTMLElement | null>,
+) {
+  const containerWidth = computed(() =>
+    container.value ? `${container.value.clientWidth}px` : "auto",
+  );
 
-    const mapContainerClass = {
-        sm: "h-24 text-sm",
-        md: "h-32 text-base",
-        lg: "h-40 text-lg",
-      };
+  const mapContainerClass = {
+    sm: "h-24 text-sm",
+    md: "h-32 text-base",
+    lg: "h-40 text-lg",
+  };
 
-    const staticContainerClass = `flex justify-between w-fit items-center px-10 border-1 text-core-black rounded gap-8 text-left has-[:focus]:border-blue-600`;
-    
-    const computedContainerClass = computed(() => [
-      mapContainerClass[props.size!],
-      evaluateProps(),
-    ]);
-    
-    const computedLabelClass = computed(() => [
-      props.disabled ? "text-grey-300" : "text-core-black",
-    ]);
-    
-    const staticInputClass = `peer w-full bg-transparent border-0 focus:outline-none focus:ring-0 cursor-pointer`;
-    
-    const computedHelpClass = computed(() => [
-      props.size === "sm" ? "text-xs" : "",
-      props.size === "md" ? "text-sm" : "",
-      props.size === "lg" ? "text-base" : "",
-      props.disabled ? "text-grey-300" : "text-grey-500",
-    ]);
-    const computedErrorClass = computed(() => [
-      props.size === "sm" ? "text-xs" : "",
-      props.size === "md" ? "text-sm" : "",
-      props.size === "lg" ? "text-base" : "",
-      props.disabled ? "text-grey-300" : "text-core-black",
-    ]);
-    
-    const evaluateProps = () => {
-      switch (true) {
-        case props.disabled:
-          return "bg-grey-100 border-grey-100 text-grey-300 cursor-not-allowed";
-        case props.error:
-          return "border-semantic-error bg-white text-core-black cursor-pointer ";
-        default:
-          return "border-grey-300 bg-white text-core-black cursor-pointer";
-      }
-    };
+  const staticContainerClass = `flex justify-between w-full items-center px-10 border-1 rounded gap-8 text-left has-[:focus]:border-blue-600`;
 
-    return {
-        staticContainerClass,
-        computedContainerClass,
-        computedLabelClass,
-        staticInputClass,
-        computedHelpClass,
-        computedErrorClass,
-        containerWidth
-    };
+  const computedContainerClass = computed(() => [
+    mapContainerClass[props.size!],
+    evaluateProps(),
+  ]);
+
+  const computedLabelClass = computed(() => [
+    props.disabled ? "text-grey-300" : "text-core-black",
+  ]);
+
+  const staticInputClass = `peer w-full bg-transparent border-0 focus:outline-none focus:ring-0 cursor-text truncate`;
+
+  const computedHelpClass = computed(() => [
+    props.size === "sm" ? "text-xs" : "",
+    props.size === "md" ? "text-sm" : "",
+    props.size === "lg" ? "text-base" : "",
+    props.disabled ? "text-grey-300" : "text-grey-500",
+  ]);
+  const computedErrorClass = computed(() => [
+    props.size === "sm" ? "text-xs" : "",
+    props.size === "md" ? "text-sm" : "",
+    props.size === "lg" ? "text-base" : "",
+    props.disabled ? "text-grey-300" : "text-core-black",
+  ]);
+
+  const evaluateProps = () => {
+    switch (true) {
+      case props.disabled:
+        return "bg-grey-100 border-grey-100 text-grey-300 cursor-not-allowed";
+      case props.error:
+        return "border-semantic-error bg-white text-core-black cursor-text";
+      default:
+        return "border-grey-300 bg-white text-core-black cursor-text";
+    }
+  };
+
+  return {
+    staticContainerClass,
+    computedContainerClass,
+    computedLabelClass,
+    staticInputClass,
+    computedHelpClass,
+    computedErrorClass,
+    containerWidth,
+  };
 }
