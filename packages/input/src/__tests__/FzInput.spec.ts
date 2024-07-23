032d6de0d7152a02ac78ff5ab3a2e4d497f17027
Cristian Barraco <cristian.barraco96@gmail.com>
2024-07-23 15:46:44 +0200

    chore(LIB-541): eslint

--- packages/input/src/__tests__/FzInput.spec.ts
+++ packages/input/src/__tests__/FzInput.spec.ts
@@ -1,14 +1,14 @@
-import { describe, it, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
-import { FzInput } from '..'
+import { describe, it, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import { FzInput } from "..";
 
-describe.concurrent('FzInput', () => {
-  it('matches snaphost', async ({ expect }) => {
+describe.concurrent("FzInput", () => {
+  it("matches snaphost", async ({ expect }) => {
     const wrapper = mount(FzInput, {
       props: {},
-      slots: {}
-    })
+      slots: {},
+    });
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
--- packages/input/src/types.ts
+++ packages/input/src/types.ts
@@ -1,48 +1,48 @@
 type FzInputProps = {
-    /**
-     * The label displayed on top of the input
-     */
-    label: string;
-    /**
-     * The size of the input
-     */
-    size?: "sm" | "md" | "lg";
-    /**
-     * The placeholder displayed in the input
-     */
-    placeholder?: string;
-    /**
-     * If set to true, the input is required
-     */
-    required?: boolean;
-    /**
-     * If set to true, the input is disabled
-     */
-    disabled?: boolean;
-    /**
-     * If set to true, the input is in error state
-     */
-    error?: boolean;
-    /**
-     * Left icon name
-     */
-    leftIcon?: string;
-    /**
-     * Right icon name
-     */
-    rightIcon?: string;
-    /**
-     * The input type
-     */
-    type?: 'text' | 'password' | 'email' | 'number' | 'tel' | 'url';
-    /**
-     * If set to true, the input is valid
-     */
-    valid?: boolean
-    /**
-     * Pattern to validate the input
-     */
-    pattern?: string;
-}
+  /**
+   * The label displayed on top of the input
+   */
+  label: string;
+  /**
+   * The size of the input
+   */
+  size?: "sm" | "md" | "lg";
+  /**
+   * The placeholder displayed in the input
+   */
+  placeholder?: string;
+  /**
+   * If set to true, the input is required
+   */
+  required?: boolean;
+  /**
+   * If set to true, the input is disabled
+   */
+  disabled?: boolean;
+  /**
+   * If set to true, the input is in error state
+   */
+  error?: boolean;
+  /**
+   * Left icon name
+   */
+  leftIcon?: string;
+  /**
+   * Right icon name
+   */
+  rightIcon?: string;
+  /**
+   * The input type
+   */
+  type?: "text" | "password" | "email" | "number" | "tel" | "url";
+  /**
+   * If set to true, the input is valid
+   */
+  valid?: boolean;
+  /**
+   * Pattern to validate the input
+   */
+  pattern?: string;
+};
 
-export { FzInputProps }
+export { FzInputProps };
