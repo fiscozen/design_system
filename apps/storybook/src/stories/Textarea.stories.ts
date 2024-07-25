ff9bef80c64e0782125e6bb67e98038649eff15f
Alen Ajam <alen@fiscozen.it>
2024-07-25 14:20:58 +0200

    LIB-583 feat(textarea): add FzTextarea (#91)
    * LIB-583 feat(textarea): add FzTextarea
    
    * LIB-583 docs(textarea): document props
    
    * LIB-583 fix(textarea): add model generic type

---
+++ apps/storybook/src/stories/Textarea.stories.ts
@@ -0,0 +1,89 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { FzTextarea } from '@fiscozen/textarea'
+import { errorMessages } from 'vue/compiler-sfc'
+
+const meta: Meta<typeof FzTextarea> = {
+  title: '@fiscozen/textarea/FzTextarea',
+  component: FzTextarea,
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg']
+    },
+    resize: {
+      control: 'select',
+      options: ['none', 'vertical', 'horizontal', 'all']
+    }
+  },
+  args: {
+    label: 'This is a label',
+    placeholder: 'This is a placeholder',
+    modelValue: ''
+  },
+  decorators: [() => ({
+    template: '<div class="p-10"><story/></div>'
+  })]
+}
+
+type Story = StoryObj<typeof meta>
+
+const Default: Story = {
+  args: {
+    id: 'default'
+  }
+}
+
+const WithValue: Story = {
+  args: {
+    id: 'default',
+    modelValue: 'This is the initial value'
+  }
+}
+
+const Small: Story = {
+  args: {
+    id: 'small',
+    size: 'sm'
+  }
+}
+
+const Large: Story = {
+  args: {
+    id: 'large',
+    size: 'lg'
+  }
+}
+
+const Error: Story = {
+  args: {
+    id: 'error',
+    error: true,
+    errorMessage: 'This is an error message'
+  }
+}
+
+const Help: Story = {
+  args: {
+    id: 'help',
+    helpMessage: 'This is a long long long long long long long long long long help message'
+  }
+}
+
+const Valid: Story = {
+  args: {
+    id: 'valid',
+    valid: true
+  }
+}
+
+const Disabled: Story = {
+  args: {
+    id: 'disabled',
+    disabled: true
+  }
+}
+
+export { Default, WithValue, Small, Large, Error, Help, Valid, Disabled }
+
+export default meta
---
+++ packages/textarea/src/types.ts
@@ -0,0 +1,72 @@
+type FzTextareaProps = {
+  /**
+   * Uniquely identifies textarea and binds label to it
+   */
+  id?: string;
+  /**
+   * Defines the textarea key in a form
+   */
+  name?: string;
+  /**
+   * Size of the textarea
+   */
+  size?: "sm" | "md" | "lg";
+  /**
+   * A message shown above the textarea describing what to insert
+   */
+  label: string;
+  /**
+   * Whether a value is required
+   */
+  required?: boolean;
+  /**
+   * A message shown inside the textarea to guide the user
+   */
+  placeholder?: string;
+  /**
+   * Signals an error to the user
+   */
+  error?: boolean;
+  /**
+   * A message of error shown under the textarea if error is true
+   */
+  errorMessage?: string;
+  /**
+   * Disables any user interaction
+   */
+  disabled?: boolean;
+  /**
+   * Whether and how the user can resize
+   */
+  resize?: "none" | "vertical" | "horizontal" | "all";
+  /**
+   * Number of rows that fit in the textarea
+   */
+  rows?: number;
+  /**
+   * Number of characters that horizontally fit in the textarea
+   */
+  cols?: number;
+  /**
+   * A message shown under the textarea
+   */
+  helpMessage?: string;
+  /**
+   * Whether to toggle the green check icon
+   */
+  valid?: boolean;
+  /**
+   * Permitted minimum length
+   */
+  minlength?: number;
+  /**
+   * Permitted maximum length
+   */
+  maxlength?: number;
+  /**
+   * If true, cannot be edited but can still be focused
+   */
+  readonly?: boolean;
+};
+
+export { FzTextareaProps };
