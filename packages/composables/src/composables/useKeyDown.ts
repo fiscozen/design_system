d14abd5a09d71a3b91aa951dab04b10765a556d1
Alen Ajam <alen@fiscozen.it>
2024-08-01 11:02:03 +0200

    LIB-601 feat(dropdown): ESC press closes dropdown (#102)
    * LIB-601 feat(dropdown): ESC press closes dropdown
    
    * chore: patch composables and dropdown

---
+++ packages/composables/src/composables/useKeyDown.ts
@@ -0,0 +1,26 @@
+import { onBeforeUnmount, onMounted, Ref } from 'vue'
+
+function useKeyDown(component: Ref<HTMLElement | undefined>, callback: (event: KeyboardEvent) => void) {
+  // fail early if any of the required params is missing
+  if (!component) {
+    throw new Error('A target component has to be provided.')
+  }
+
+  if (!callback || typeof callback !== 'function') {
+    throw new Error('A callback has to be provided.')
+  }
+
+  const listener = (event: KeyboardEvent) => {
+    callback(event)
+  }
+
+  onMounted(() => {
+    component.value!.addEventListener('keydown', listener)
+  })
+
+  onBeforeUnmount(() => {
+    component.value!.removeEventListener('keydown', listener)
+  })
+}
+
+export { useKeyDown }
