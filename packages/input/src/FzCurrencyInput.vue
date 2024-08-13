84b070ad08bb624f1c070e059a0ecad835903c49
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-08-13 11:21:43 +0200

    wip

---
+++ packages/input/src/FzCurrencyInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <FzInput
+        :props
+    ></FzInput>
+</template>
+
+<script>
+import FzInput from './FzInput.vue'
+import {FzCurrencyInputProps} from './types'
+
+const props = withDefaults(defineProps<FzCurrencyInputProps>(), {
+    decimalSeparator: ','
+})
+const emit = defineEmits(['input', 'focus']);
+</script>
