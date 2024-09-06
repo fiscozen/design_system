bca4a35b088842a97e2f500b0baccc3229478a61
Cristian Barraco <cristian.barraco96@gmail.com>
2024-09-06 11:39:41 +0200

    feat(LIB-834): Update icon package. minor fix on icon list story

--- apps/storybook/src/stories/Icon.stories.ts
+++ apps/storybook/src/stories/Icon.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import { FzIcon } from '@fiscozen/icons'
 import {all} from '@awesome.me/kit-8137893ad3/icons'
 import { FzButton } from '@fiscozen/button'
+import { FzToastQueue } from '@fiscozen/toast'
 
 const meta: Meta<typeof FzIcon> = {
   title: '@fiscozen/icon/FzIcon',
@@ -35,7 +36,7 @@ export const Primary: Story = {}
 
 export const PreviewAllIcons: Story = {
   render: () => ({
-    components: { FzIcon, FzButton },
+    components: { FzIcon, FzButton, FzToastQueue },
     setup() {
       const mappedIconsByPrefix : {
         [key:string]: string[]
@@ -45,7 +46,8 @@ export const PreviewAllIcons: Story = {
         if (!mappedIconsByPrefix[icon.prefix]) {
           mappedIconsByPrefix[icon.prefix] = []
         }
-        mappedIconsByPrefix[icon.prefix].push(icon.iconName)
+        if(!mappedIconsByPrefix[icon.prefix].includes(icon.iconName)) 
+          mappedIconsByPrefix[icon.prefix].push(icon.iconName)
       })
 
       return {
@@ -63,21 +65,22 @@ export const PreviewAllIcons: Story = {
         document.body.removeChild(el);
 
         // show toast
-        alert('Copied to clipboard: <FzIcon :name="' + icon + '" size=\"lg\" :variant="'+variant+'"/>');
+        alert('Copied to clipboard: <FzIcon name="' + icon + '" size=\"lg\" variant="'+variant+'"/>');
       }
     },
     template: `
       <div>
-        <h1 class='font-bold px-10'>Icon List</h1>
-        <div v-for="prefix in Object.keys(mappedIconsByPrefix)" :key="prefix" class="flex flex-row p-10">
-          <h2>Variant: <b>{{prefix}}</b></h2>
+        <h1 class='font-bold px-10 text-3xl mb-10'>Icon List</h1>
+        <p class='px-10'> Click on the icon to copy the code </p>
+        <div v-for="prefix in Object.keys(mappedIconsByPrefix)" :key="prefix" class="flex flex-col p-10 gap-10 mb-10">
+          <h2 class="text-xl">Variant: <b>{{prefix}}</b></h2>
           <div class='flex flex-row flex-wrap gap-20 overflow-hidden mt-20'>
             <div v-for="icon in mappedIconsByPrefix[prefix]" 
                 :key="icon" 
-                class='p-10 gap-4 rounded-base border-1 flex items-center flex-col justify-center w-128 h-128'>
+                @click="copyCode(icon, prefix)"
+                class='p-10 gap-4 rounded-base border-1 flex items-center flex-col justify-center w-128 h-128 hover:bg-grey-100 cursor-pointer'>
               <FzIcon :name="icon" size="lg" :variant="prefix" />
               <span class='text-xs'>{{icon}} {{filter}}</span>
-              <FzButton @click="copyCode(icon, prefix)" variant="secondary" size='sm'>Copy</FzButton>
             </div>
           </div>
         </div>
