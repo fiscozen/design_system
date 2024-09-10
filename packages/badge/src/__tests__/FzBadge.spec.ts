b2596a71f27dcd8995f91c3bfb4ac098efe7c914
Cristian Barraco <cristian.barraco96@gmail.com>
2024-09-10 14:57:51 +0200

    feat(LIB-726): add test and update snapshot

--- packages/badge/src/__tests__/FzBadge.spec.ts
+++ packages/badge/src/__tests__/FzBadge.spec.ts
@@ -67,4 +67,24 @@ describe.concurrent("FzBadge", () => {
 
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it("should have rounded-full class when default slot is a string with 1 character", async ({ expect }) => {
+    const wrapper = mount(FzBadge, {
+      slots: {
+        default: "1",
+      },
+    });
+
+    expect(wrapper.classes()).toContain("rounded-full");
+  });
+
+  it("should not have rounded-full class when default slot is a string with more than 1 character", async ({ expect }) => {
+    const wrapper = mount(FzBadge, {
+      slots: {
+        default: "Fiscozen",
+      },
+    });
+
+    expect(wrapper.classes()).not.toContain("rounded-full");
+  });
 });
