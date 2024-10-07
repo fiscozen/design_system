13345e4ca9ea4bbbd30e8ce7e3f096d26662663e
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-10-07 17:27:42 +0200

    LIB-1294 fix(input): propage model change into fzinput

--- packages/input/src/FzCurrencyInput.vue
+++ packages/input/src/FzCurrencyInput.vue
@@ -1,14 +1,15 @@
 <template>
-  <FzInput ref="fzInputRef" v-bind="props" type="text"></FzInput>
+  <FzInput ref="fzInputRef" v-bind="props" :modelValue="fzInputModel" type="text"></FzInput>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { computed, nextTick, onMounted, ref, watch } from "vue";
 import FzInput from "./FzInput.vue";
 import { FzCurrencyInputProps } from "./types";
 import { useCurrency } from "@fiscozen/composables";
 
 const fzInputRef = ref<InstanceType<typeof FzInput>>();
+const fzInputModel = ref();
 const containerRef = computed(() => fzInputRef.value?.containerRef);
 const inputRef = computed(() => fzInputRef.value?.inputRef);
 const props = defineProps<FzCurrencyInputProps>();
@@ -18,8 +19,15 @@ defineEmits(["update:amount"]);
 
 onMounted(() => {
   currencyInputRef.value = inputRef.value;
+  nextTick(() => {
+    fzInputModel.value = inputRef.value?.value;
+  })
 });
 const model = defineModel("amount");
+
+watch(model, (newVal) => {
+  fzInputModel.value = newVal as string
+})
 
 defineExpose({
   inputRef,
