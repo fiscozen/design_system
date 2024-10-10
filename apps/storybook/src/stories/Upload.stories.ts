8a27c7c82e6c3f35d31bdb31c74e6c2e264ee0bb
Alen Ajam <alen@fiscozen.it>
2024-10-10 12:00:03 +0200

    LIB-1301 feat(upload): add FzUpload

---
+++ apps/storybook/src/stories/Upload.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { FzUpload } from '@fiscozen/upload'
+
+const meta: Meta<typeof FzUpload> = {
+  title: '@fiscozen/upload/FzUpload',
+  component: FzUpload,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {
+    id: 'example-id'
+  },
+  decorators: [() => ({
+    template: '<div class="p-16 max-w-[400px]"><story/></div>'
+  })]
+}
+
+type Story = StoryObj<typeof meta>
+
+const Default: Story = {
+  args: {}
+}
+
+export { Default }
+
+export default meta
---
+++ packages/upload/src/types.ts
@@ -0,0 +1,9 @@
+type FzUploadProps = {
+  /** Unique identifier of the input */
+  id?: string;
+  name?: string;
+  multiple?: boolean;
+  accept?: string;
+};
+
+export { FzUploadProps };
