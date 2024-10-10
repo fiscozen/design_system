3a9ae837c799d44b5d6c6e1003f386b41baf41fd
Alen Ajam <alen@fiscozen.it>
2024-10-10 15:30:22 +0200

    LIB-1301 docs(upload): add prop comments

--- packages/upload/src/types.ts
+++ packages/upload/src/types.ts
@@ -1,8 +1,11 @@
 type FzUploadProps = {
   /** Unique identifier of the input */
   id?: string;
+  /** Name of the input */
   name?: string;
+  /** Whether user can upload multiple files at once */
   multiple?: boolean;
+  /** Pattern that dictates what files the input accepts */
   accept?: string;
 };
 
