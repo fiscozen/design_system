ba9521790d6747a9026e0474157cf49dd658ab51
Alen Ajam <alen@fiscozen.it>
2024-10-14 11:54:16 +0200

    LIB-1301 feat(upload): FzUpload (#150)
    * LIB-1302 feat(icons): add cloud-arrow-up icon
    
    * LIB-1301 feat(upload): add FzUpload
    
    * LIB-1301 fix(upload): add key to list item
    
    Co-authored-by: Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
    
    * LIB-1301 docs(upload): add prop comments
    
    * LIB-1301 feat(upload): add size prop
    
    * LIB-1301 refactor(upload): add label props
    
    * LIB-1301 test(upload): add two tests
    
    * chore: update lock
    
    * chore: revert button changes
    
    * chore(upload): format
    
    * chore(upload): add v-model bind to story
    
    ---------
    
    Co-authored-by: Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>

---
+++ packages/upload/src/FzUpload.vue
@@ -0,0 +1,123 @@
+<template>
+  <div :class="{ 'text-sm': size === 'sm', 'text-md': size === 'md' }">
+    <div
+      class="relative flex items-center gap-8 p-12 border-1 border-dashed rounded border-grey-300 bg-background-alice-blue"
+      @drop="handleDrop"
+      @dragover="$event.preventDefault()"
+    >
+      <input
+        ref="input"
+        type="file"
+        class="hidden"
+        :id
+        :name
+        :multiple
+        :accept
+        @change="handleInputChange"
+      />
+      <FzButton
+        ref="fzButton"
+        variant="secondary"
+        iconName="cloud-arrow-up"
+        iconPosition="before"
+        iconVariant="fas"
+        class="select-none"
+        :size
+        @click="input?.click()"
+      >
+        {{ buttonLabel }}
+      </FzButton>
+      {{ dragAndDropLabel }}
+    </div>
+
+    <ul v-if="model?.length" class="mt-8 border-1 border-grey-300 rounded px-8">
+      <li
+        v-for="file in model"
+        class="border-b-1 last:border-b-0 py-8 flex items-center justify-between"
+        :key="getFileUrl(file)"
+      >
+        <FzLink
+          :size="size === 'sm' ? 'xs' : 'sm'"
+          :to="getFileUrl(file)"
+          external
+          target="_blank"
+          class="truncate block"
+        >
+          {{ file.name }}
+        </FzLink>
+
+        <FzIconButton
+          iconName="xmark-circle"
+          variant="invisible"
+          :size="size === 'sm' ? 'xs' : 'sm'"
+          @click="deleteFile(file)"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onUnmounted, ref } from "vue";
+import { FzUploadProps } from "./types";
+import { FzButton, FzIconButton } from "@fiscozen/button";
+import { FzLink } from "@fiscozen/link";
+
+const props = withDefaults(defineProps<FzUploadProps>(), {
+  size: "md",
+  buttonLabel: "Carica",
+  dragAndDropLabel: "o trascina qui",
+});
+const emit = defineEmits([]);
+const model = defineModel<File[]>();
+
+const input = ref<HTMLInputElement | null>(null);
+const urlByFileMap = ref(new Map<File, string>());
+
+onUnmounted(() => {
+  urlByFileMap.value.forEach((value) => {
+    window.URL.revokeObjectURL(value);
+  });
+});
+
+function handleDrop(event: DragEvent) {
+  event.preventDefault();
+  if (!event.dataTransfer) return;
+  const draggedFiles = [...event.dataTransfer?.items]
+    .filter((item) => item.kind === "file")
+    .map((item) => item.getAsFile())
+    .filter(Boolean) as File[];
+  addFiles(draggedFiles);
+}
+
+function handleInputChange(event: Event) {
+  if (!input.value?.files) return;
+  addFiles([...input.value.files]);
+  input.value.value = "";
+}
+
+function addFiles(filesToAdd: File[]) {
+  model.value = [...(model.value ?? []), ...filesToAdd];
+}
+
+function getFileUrl(file: File) {
+  let url = urlByFileMap.value.get(file);
+  if (!url) {
+    url = window.URL.createObjectURL(file);
+    urlByFileMap.value.set(file, url);
+  }
+  return url;
+}
+
+function deleteFile(file: File) {
+  if (!model.value?.length) return;
+  const index = model.value?.indexOf(file);
+  if (index !== -1) {
+    const newArr = [...model.value];
+    newArr.splice(index, 1);
+    model.value = newArr;
+  }
+}
+</script>
+
+<style scoped></style>
