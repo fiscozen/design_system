abef88a3c1b0df2393fdbcba791106a086292424
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-10-16 15:57:42 +0200

    LIB-1266 fix(typeahead): new design wip

--- packages/typeahead/src/types.ts
+++ packages/typeahead/src/types.ts
@@ -1,9 +1,16 @@
 import { FzSelectProps, FzSelectOptionsProps } from "@fiscozen/select";
 import { FzInputProps } from "@fiscozen/input";
+import { IconVariant } from "@fiscozen/icons";
 
 interface FzTypeaheadProps {
-  selectProps: FzSelectProps;
-  inputProps: FzInputProps;
+  /**
+   * @deprecated
+   */
+  selectProps?: FzSelectProps;
+  /**
+   * @deprecated
+   */
+  inputProps?: FzInputProps;
   /**
    * If true, writing in the input will filter the options
    */
@@ -12,8 +19,66 @@ interface FzTypeaheadProps {
    * @deprecated use `selectProps.options` instead
    */
   filteredOptions?: FzSelectOptionsProps[];
+  options?: FzSelectOptionsProps[];
   filterFn?: (text?: string) => FzSelectOptionsProps[];
   delayTime?: number;
+  /**
+   * Left icon name
+   */
+  leftIcon?: string;
+  /**
+   * Left icon variant
+   */
+  leftIconVariant?: IconVariant;
+  /**
+   * Right icon name
+   */
+  rightIcon?: string;
+  /**
+   * Right icon variant
+   */
+  rightIconVariant?: IconVariant;
+  /**
+   * The size of the typeahead
+   */
+  size?: "sm" | "md" | "lg";
+  /**
+   * The label displayed on top of the input
+   */
+  label: string;
+  /**
+   * The placeholder displayed in the input
+   */
+  placeholder?: string;
+  /**
+   * If set to true, the input is valid
+   */
+  valid?: boolean;
+  /**
+   * sets input state to error and shows the message
+   */
+  errorMessage?: string;
+  /**
+   * marked as read only (no input allowed)
+   */
+  readonly?: boolean;
+  /**
+   * If set to true, the input is disabled
+   */
+  disabled?: boolean;
+  /**
+   * If set to true, the input is required
+   */
+  required?: boolean;
+  /**
+   * This text will be displayed at the bottom of the input
+   * to give a description or context.
+   */
+  helpText?: string;
+  /**
+   * Default empty search behavior
+   */
+  emptySearchNoFilter?: boolean;
 }
 
 export { FzTypeaheadProps };
