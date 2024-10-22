f9cfdb74039ecc877586c808d0244b9e8863fa51
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-10-22 15:29:32 +0200

    LIB-1266 fix(typeahead): version and cleanup

--- packages/select/src/common.ts
+++ packages/select/src/common.ts
@@ -1,16 +1,15 @@
 export const MIN_WIDTH = 240;
 
 export function calculateContainerWidth(opener: HTMLElement) {
-    const rect = opener.getBoundingClientRect();
-  
-    const minWidth = rect.width > MIN_WIDTH ? rect.width : MIN_WIDTH;
-    const spaceOnRight = window.innerWidth - (rect.right + window.scrollX);
-    const spaceOnLeft = rect.left + window.scrollX;
-    const maxWidth = rect.width + Math.max(spaceOnRight, spaceOnLeft);
-  
+  const rect = opener.getBoundingClientRect();
 
-    return {
-        minWidth,
-        maxWidth
-    };
-  }
+  const minWidth = rect.width > MIN_WIDTH ? rect.width : MIN_WIDTH;
+  const spaceOnRight = window.innerWidth - (rect.right + window.scrollX);
+  const spaceOnLeft = rect.left + window.scrollX;
+  const maxWidth = rect.width + Math.max(spaceOnRight, spaceOnLeft);
+
+  return {
+    minWidth,
+    maxWidth,
+  };
+}
