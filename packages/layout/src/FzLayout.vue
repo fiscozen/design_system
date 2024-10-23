94d687118176f5a110237cf1adec791a2799d814
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-10-23 12:49:37 +0200

    wip

--- packages/layout/src/FzLayout.vue
+++ packages/layout/src/FzLayout.vue
@@ -1,4 +1,14 @@
 <template>
+    <div class="fz-layout w-dvw h-dvh grid" :class="layoutClass">
+        <template v-if="props.layout === 'twoColumns'">
+            <!--div><slot name="fz-col-left"></slot></div>
+            <div><slot name="fz-col-right"></slot></div-->
+            <slot></slot>
+        </template>
+        <template v-if="props.layout === 'multipleRows'">
+            <slot></slot>
+        </template>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -8,6 +18,22 @@ import { FzLayoutProps } from './types'
 const props = withDefaults(defineProps<FzLayoutProps>(), {})
 
 const emit = defineEmits([])
+
+const layoutClass = computed(() => {
+    let res = '';
+    switch (props.layout) {
+        case 'multipleRows':
+            res = 'grid-rows-auto'
+            break;
+        case 'twoColumns':
+            res = 'grid-cols-2'
+            break;
+        default:
+            break;
+    }
+    return res;
+});
+
 </script>
 
 <style scoped>
--- packages/layout/src/types.ts
+++ packages/layout/src/types.ts
@@ -1,3 +1,5 @@
-type FzLayoutProps = {}
+type FzLayoutProps = {
+    layout: 'twoColumns' | 'multipleRows'
+}
 
 export { FzLayoutProps }
