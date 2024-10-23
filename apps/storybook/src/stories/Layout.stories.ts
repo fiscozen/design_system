929dfbeacf7a6d315fb76c90aff742169188cbbc
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2024-10-23 12:49:37 +0200

    wip

--- apps/storybook/src/stories/Layout.stories.ts
+++ apps/storybook/src/stories/Layout.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { FzLayout } from '@fiscozen/layout'
+import { FzLayout, FzLayoutProps } from '@fiscozen/layout'
 
 const meta: Meta<typeof FzLayout> = {
   title: '@fiscozen/layout/FzLayout',
@@ -7,15 +7,62 @@ const meta: Meta<typeof FzLayout> = {
   tags: ['autodocs'],
   argTypes: {},
   args: {},
-  decorators: []
+  decorators: [
+    () => ({
+      template: `
+      <div class="opacity">
+        <div>
+          <story /> 
+        </div>
+      </div>
+      `
+    })
+  ]
 }
 
 type Story = StoryObj<typeof meta>
 
-const Default: Story = {
-  args: {}
+const twoColumns = (args: FzLayoutProps) => ({
+  setup() {
+    return { args }
+  },
+  components: {FzLayout},
+  template: `
+    <FzLayout v-bind="args">
+      <div class="w-full h-full bg-red-100"></div>
+      <div class="w-full h-full bg-green-100"></div>
+    </FzLayout>
+  `
+})
+
+export const TwoColumns = {
+  render: twoColumns,
+  args: {
+    layout: 'twoColumns'
+  }
 }
 
-export { Default }
 
+const multipleRows = (args: FzLayoutProps) => ({
+  setup() {
+    return { args }
+  },
+  components: {FzLayout},
+  template: `
+    <FzLayout v-bind="args">
+      <div class="w-full bg-red-100"></div>
+      <div class="w-full bg-green-100"></div>
+      <div class="w-full bg-orange-200"></div>
+      <div class="w-full bg-cyan-100"></div>
+    </FzLayout>
+  `
+})
+
+
+export const MultipleRows = {
+  render: multipleRows,
+  args: {
+    layout: 'multipleRows'
+  }
+}
 export default meta
