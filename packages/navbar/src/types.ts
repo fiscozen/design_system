422589a8d6816d9b8ff3c0ceeebd06852d44bcb7
Alen Ajam <alen@fiscozen.it>
2024-11-04 13:05:37 +0100

    feat(navbar): add event emitter menuButtonClick (#172)

--- packages/navbar/src/types.ts
+++ packages/navbar/src/types.ts
@@ -1,8 +1,14 @@
 export type FzNavbarVariant = 'horizontal' | 'vertical'
 
-export interface FzNavbarProps {
+interface FzNavbarProps {
   /**
    * The main direction of the navbar
    */
   variant: FzNavbarVariant
 }
+
+type FzNavbarEmits = {
+  'fznavbar:menuButtonClick': []
+}
+
+export type { FzNavbarProps, FzNavbarEmits }
