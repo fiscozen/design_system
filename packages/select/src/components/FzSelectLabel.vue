29570094dc7ee0dbe40b929158823e18ea0ba7e6
Cristian Barraco <cristian.barraco@fiscozen.it>
2024-11-04 12:24:25 +0100

    feat(LIB-1360): add Label on FzSelect (#170)
    * feat(LIB-1360): add Label on FzSelect
    
    * feat(LIB-1360): test and cleanup code
    
    * fix(LIB-1360): pr comments
    
    * update version
    
    * label instead of button
    
    * test fix

---
+++ packages/select/src/components/FzSelectLabel.vue
@@ -0,0 +1,33 @@
+<template>
+  <label
+    disabled
+    test-id="fzselect-label"
+    @click.prevent.stop
+    :title="option.label"
+    :class="computedClass"
+  >
+    {{ option.label }}
+  </label>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { FzSelectLabelProps } from "../types";
+
+const props = defineProps<{
+  option: FzSelectLabelProps;
+  size: "sm" | "md" | "lg";
+}>();
+
+const staticClass =
+  "text-grey-400 flex items-center text-ellipsis whitespace-nowrap";
+
+const mappedClass = {
+  sm: "text-xs h-24 px-12",
+  md: "text-sm h-32 px-14",
+  lg: "text-md h-40 px-16",
+};
+const computedClass = computed(() => {
+  return [staticClass, mappedClass[props.size]];
+});
+</script>
