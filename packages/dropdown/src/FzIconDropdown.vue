ed5f61c765efd725862d329223743b9fc017ca0f
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-11-06 15:52:18 +0100

    LIB-1368 feat(iconDropdown): icon dropdown component

--- packages/dropdown/src/FzIconDropdown.vue
+++ packages/dropdown/src/FzIconDropdown.vue
@@ -1,17 +1,24 @@
 <template>
-  <FzDropdown v-bind="props">
-    <template #opener="slotProps">
-        <FzIconButton :iconName="iconName" @click="slotProps.isOpen = !slotProps.isOpen"/>
+  <FzDropdown v-bind="props" ref="dropdown">
+    <template #opener>
+        <FzIconButton :iconName="iconName" @click="dropdown?.open()" :variant="buttonVariant"/>
     </template>
   </FzDropdown>
 </template>
 
 <script setup lang="ts">
+import { ComponentPublicInstance, ref } from 'vue';
 import FzDropdown from './FzDropdown.vue';
-import { FzIconDropdownProps } from './types';
+import { type FzIconDropdownProps } from './types';
 import { FzIconButton } from '@fiscozen/button';
 
+const dropdown = ref<ComponentPublicInstance>();
+
 const props = withDefaults(defineProps<FzIconDropdownProps>(), {
-    iconName: 'bars'
+    iconName: 'bars',
+    closeOnActionClick: true,
+    align: 'center',
+    teleport: true,
+    buttonVariant: 'secondary'
 })
 </script>
