52b8d1e899c636e1a061b26414804e68da7ab9e1
Cristian Barraco <cristian.barraco@fiscozen.it>
2024-11-12 16:18:16 +0100

    feat: Improved storybook graphics and logo (#161)
    * feat: Improved storybook graphics and logo
    
    * downgrade plugins to correct storybook version

---
+++ apps/storybook/.storybook/FiscozenTheme.ts
@@ -0,0 +1,13 @@
+import {create} from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Fiscozen',
+  brandUrl: 'https://fiscozen.it',
+  brandImage: 'https://www.fiscozen.it/site/uploads/2020/11/fiscozen-logo-black.svg',
+  appBg: '#F7F6F3',
+  textColor: '#2c282f',
+  barTextColor: '#00001E',
+  colorSecondary: '#5A6EFF',
+  appBorderColor: '#D1DDE6'
+})
