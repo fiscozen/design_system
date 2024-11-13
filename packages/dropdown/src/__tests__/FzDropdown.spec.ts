9b36efe6f99aa44cc3d5df35bb79d523be7698d1
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-11-13 10:11:23 +0100

    LIB-1368 tests(iconDropdown): basic tests

--- packages/dropdown/src/__tests__/FzDropdown.spec.ts
+++ packages/dropdown/src/__tests__/FzDropdown.spec.ts
@@ -1,8 +1,9 @@
 import { beforeEach, describe, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { FzDropdown } from '..'
+import { FzDropdown, FzIconDropdown } from '..'
+import { FzIconButton } from '@fiscozen/button'
 
-describe.concurrent('FzTopbar', () => {
+describe.concurrent('FzDropdown', () => {
   beforeEach(() => {
     const mockIntersectionObserver = vi.fn()
     mockIntersectionObserver.mockReturnValue({
@@ -40,6 +41,38 @@ describe.concurrent('FzTopbar', () => {
       }
     })
 
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('matches snapshot', async ({ expect }) => {
+    const wrapper = mount(FzIconDropdown, {
+      props: {
+        actionsLabel: 'This is the items label',
+        actions: [
+          {
+            label: 'Item #1',
+            disabled: true,
+            type: 'button'
+          },
+          {
+            label: 'Item #2',
+            type: 'button'
+          }
+        ]
+      },
+      slots: {
+        default: 'This is a dropdown'
+      }
+    })
+
+    await wrapper.vm.$nextTick()
+
+    const iconButton = wrapper.findComponent(FzIconButton)
+    expect(iconButton.exists()).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+
+    iconButton.trigger('click')
+    await wrapper.vm.$nextTick()
     expect(wrapper.html()).toMatchSnapshot()
   })
 })
