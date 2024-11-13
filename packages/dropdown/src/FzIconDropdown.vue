4c324655552911a693ade61ce0f9ec83800f8c1b
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-11-13 10:11:23 +0100

    LIB-1368 tests(iconDropdown): basic tests

--- packages/dropdown/src/FzIconDropdown.vue
+++ packages/dropdown/src/FzIconDropdown.vue
@@ -1,24 +1,24 @@
 <template>
   <FzDropdown v-bind="props" ref="dropdown">
     <template #opener>
-        <FzIconButton :iconName="iconName" @click="dropdown?.open()" :variant="buttonVariant"/>
+      <FzIconButton :iconName="iconName" @click="dropdown?.open()" :variant="buttonVariant" />
     </template>
   </FzDropdown>
 </template>
 
 <script setup lang="ts">
-import { ComponentPublicInstance, ref } from 'vue';
-import FzDropdown from './FzDropdown.vue';
-import { type FzIconDropdownProps } from './types';
-import { FzIconButton } from '@fiscozen/button';
+import { ref } from 'vue'
+import FzDropdown from './FzDropdown.vue'
+import { type FzIconDropdownProps } from './types'
+import { FzIconButton } from '@fiscozen/button'
 
-const dropdown = ref<ComponentPublicInstance>();
+const dropdown = ref<InstanceType<typeof FzDropdown>>()
 
 const props = withDefaults(defineProps<FzIconDropdownProps>(), {
-    iconName: 'bars',
-    closeOnActionClick: true,
-    align: 'center',
-    teleport: true,
-    buttonVariant: 'secondary'
+  iconName: 'bars',
+  closeOnActionClick: true,
+  align: 'center',
+  teleport: true,
+  buttonVariant: 'secondary'
 })
 </script>
