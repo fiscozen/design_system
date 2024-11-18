d3a69d9091c6a1ede9fd88c0a1f334a8cf908a10
Cristian Barraco <cristian.barraco96@gmail.com>
2024-11-18 16:24:53 +0100

    feat: doc section improvement

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-viewport',
+    'storybook-addon-vue-mdx',
     '@chromatic-com/storybook'
   ],
   framework: {
