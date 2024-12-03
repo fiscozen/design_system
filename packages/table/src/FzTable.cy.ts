4c8d7bcd42ea2fc6916751c57a69d8082fcc51f9
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-12-03 11:52:32 +0100

    LIB-1442 feat(table): working prototype

--- packages/table/src/FzTable.cy.ts
+++ packages/table/src/FzTable.cy.ts
@@ -1,6 +1,7 @@
 import { h } from 'vue'
 import FzTable from './FzTable.vue'
 import {FzColumn} from '@fiscozen/simple-table'
+import { FzTableProps } from './types';
 
 const sampleObj = {
   nome: 'Riccardo',
@@ -28,6 +29,24 @@ const items =  [
   },
 ];
 
+const defaultTemplate = (props: FzTableProps) => () => h(
+  h('div', {class: 'h-[600px] max-w-[800px] pt-16'}, [
+    h(FzTable, props, [
+      h(FzColumn, {header: 'Nome', sticky: 'left'}),
+      h(FzColumn, {header: 'Cognome'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Email'}),
+      h(FzColumn, {header: 'Phone Number', field: 'phone_number'})
+    ])
+  ])
+)
+
 describe('<FzTable />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
@@ -40,26 +59,23 @@ describe('<FzTable />', () => {
       pages: 10,
       activePage: 2
     }
-    cy.mount(
-      () => {
-        return h(
-          h('div', {class: 'h-[600px] max-w-[800px] pt-16'}, [
-            h(FzTable, props, [
-              h(FzColumn, {header: 'Nome', sticky: 'left'}),
-              h(FzColumn, {header: 'Cognome'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Email'}),
-              h(FzColumn, {header: 'Phone Number', field: 'phone_number'})
-            ])
-          ])
-        )
-      }
-    )
+    cy.mount(defaultTemplate(props))
+    
+  })
+
+  it('shows placeholder when no data is available', () => {
+    // see: https://on.cypress.io/mounting-vue
+    const props = {
+      value: [],
+      placeholder: 'Nessun valore',
+      actions: {
+        items
+      },
+      pages: 10,
+      activePage: 2
+    }
+    cy.mount(defaultTemplate(props))
+    cy.get('.fz__table__empty').should('be.visible')
+    cy.get('.fz__table__empty').should('have.text', props.placeholder)
   })
 })
--- packages/table/src/FzTable.vue
+++ packages/table/src/FzTable.vue
@@ -6,7 +6,8 @@ import { FzIconDropdown } from '@fiscozen/dropdown';
 import { FzButton } from '@fiscozen/button';
 
 const props = withDefaults(defineProps<FzTableProps>(), {
-  pageInterval: 2
+  pageInterval: 2,
+  activePage: 0
 })
 
 const emit = defineEmits([])
@@ -68,14 +69,60 @@ const classes = computed(() => {
     case 12:
       res.push('grid-cols-12') 
       break;
-    case 13:
-      res.push('grid-cols-13') 
+    default:
+      break;
+  }
+  return res;
+})
+
+const colspan = computed(() => {
+  let res: string[] = [];
+
+  const colLength = props.actions ? columns.length + 1 : columns.length
+
+  switch (colLength) {
+    case 1:
+      res.push('col-span-1') 
+      break;
+    case 2:
+      res.push('col-span-2') 
+      break;
+    case 3:
+      res.push('col-span-3') 
+      break;
+    case 4:
+      res.push('col-span-4') 
+      break;
+    case 5:
+      res.push('col-span-5') 
+      break;
+    case 6:
+      res.push('col-span-6') 
+      break;
+    case 7:
+      res.push('col-span-7') 
+      break;
+    case 8:
+      res.push('col-span-8') 
+      break;
+    case 9:
+      res.push('col-span-9') 
+      break;
+    case 10:
+      res.push('col-span-10') 
+      break;
+    case 11:
+      res.push('col-span-11') 
+      break;
+    case 12:
+      res.push('col-span-12') 
       break;
     default:
       break;
   }
   return res;
 })
+
 const headerStaticClasses = ['fz__header__cell','sticky','top-0','z-[2]', 'bg-grey-100', 'px-16', 'h-48', 'font-medium', 'flex', 'justify-start', 'items-center']
 const headerClasses = computed(() => {
 
@@ -105,6 +152,20 @@ const updateClasses = () => {
   grid.value.style.width = `${tableWidth}px`
 }
 
+const centerPageList = computed(() => {
+  if (!props.pages) {
+    return []
+  }
+  const safeActivePage = activePage.value || 0
+  return new Array(props.pages)
+    .fill(0)
+    .map((val, index) => index)
+    .filter(el => {
+      const res = (Math.abs(el - safeActivePage) <= props.pageInterval) && el !== 0 && el !== props.pages - 1
+      return res
+    })
+})
+
 onMounted(() => {
   updateClasses()
 
@@ -117,9 +178,14 @@ onUpdated(updateClasses)
 
 <template>
 <div class="fz__table overflow-auto h-full w-full">
-  <div :class="[staticClasses, classes]" ref="grid">
+  <div
+    :class="[staticClasses, classes]"
+    ref="grid"
+    role="table"
+    :aria-rowcount="value.length"
+    :aria-colcount="columns.length">
     <template  v-for="(column, index) in columns">
-      <div :class="[headerStaticClasses, headerClasses, getBodyClasses(column, true)]">
+      <div :class="[headerStaticClasses, headerClasses, getBodyClasses(column, true)]" role="columnheader" aria-sort="none" >
          {{ column.props.header }} 
       </div>
     </template>  
@@ -128,9 +194,11 @@ onUpdated(updateClasses)
       :class="['fz__table__header--actions', headerStaticClasses, headerClasses]">
       Azioni
     </div>
-    <template v-for="row in value">
+    <template v-if="value && value.length" v-for="(row, index) in value">
       <div
+        :aria-rowindex="index"
         :class="[bodyStaticClasses, bodyClasses, getBodyClasses(column)]"
+        role="cell"
         v-for="column in columns">
         {{ row[column.props.field?.toLowerCase() || column.props.header.toLowerCase()] }}
       </div>
@@ -140,17 +208,31 @@ onUpdated(updateClasses)
         <FzIconDropdown :actions="actions.items"></FzIconDropdown>
       </div>
     </template>
+    <div :class="['fz__table__empty', 'h-full mt-80', 'justify-self-center', colspan]" v-else>
+      {{ placeholder ?? "No data available" }}
+    </div>
   </div>
+  
 </div>
-<div class="fz__table__footer w-full flex flex-row justify-end m-8">
+<div class="fz__table__footer w-full flex flex-row justify-end m-8" v-if="pages && value && value.length">
   <div class="fz__table__pagination flex flex-row justify-between items-center gap-8">
     <FzButton
-      v-for="(page, index) in pages"
-      @click="activePage = index"
-      :variant="activePage === index ? 'primary' : 'secondary'"
+      @click="activePage = 0"
+      :variant="activePage === 0 ? 'primary' : 'secondary'"
+      size="sm">1</FzButton>
+    <div class="fz__pagination__separator" v-if="activePage - pageInterval - 1 > 0">...</div>
+    <FzButton
+      v-for="page in centerPageList"
+      @click="activePage = page"
+      :variant="activePage === page ? 'primary' : 'secondary'"
       size="sm">
-        {{ index + 1 }}
+        {{ page + 1 }}
       </FzButton>
+    <div class="fz__pagination__separator" v-if="pages - activePage - pageInterval - 2 > 0">...</div>
+    <FzButton
+      @click="activePage = pages - 1"
+      :variant="activePage === pages - 1 ? 'primary' : 'secondary'"
+      size="sm">{{pages}}</FzButton>
   </div>
 </div>
 </template>
