a6b7eb1c6c9a6a87a657e600f6089e23235a94cc
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-12-04 10:42:08 +0100

    Update packages/table/src/FzTable.vue
    Co-authored-by: Alen Ajam <alen.ajam4@gmail.com>

--- packages/table/src/FzTable.vue
+++ packages/table/src/FzTable.vue
@@ -137,9 +137,9 @@ const getBodyClasses = (column: {props: FzColumnProps, children: FzColumnSlots},
   return {
     relative: !column.props.sticky,
     sticky: column.props.sticky,
-    ['left-0 z-[2]']: column.props.sticky === 'left',
-    ['z-[3]']: column.props.sticky && isHeader,
-    ['right-0']: column.props.sticky === 'right'
+    'left-0 z-[2]': column.props.sticky === 'left',
+    'z-[3]': column.props.sticky && isHeader,
+    'right-0': column.props.sticky === 'right'
   }
 }
 
