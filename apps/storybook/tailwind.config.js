a4df0157043094bf94c16fcc19917f73db35893e
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2024-12-10 12:55:42 +0100

    LIB-1442 feat(table): row handling

--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -1,18 +1,20 @@
-import tailwindConfig from '@fiscozen/style/tailwind.config';
+import tailwindConfig from '@fiscozen/style/tailwind.config'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    ...tailwindConfig,
-    content: [
-        "./src/**/*.{html,js,ts,vue}", 
-        "./node_modules/@fiscozen/**/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/badge/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/input/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/typeahead/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/datepicker/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/divider/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/composables/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/tooltip/src/**/*.{html,js,ts,vue}",
-        "./node_modules/@fiscozen/select/src/**/*.{html,js,ts,vue}"
-    ],
+  ...tailwindConfig,
+  content: [
+    './src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/**/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/badge/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/input/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/typeahead/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/datepicker/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/divider/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/composables/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/tooltip/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/select/src/**/*.{html,js,ts,vue}',
+    './node_modules/@fiscozen/table/src/**/*.{html,js,ts,vue}'
+  ]
 }
+
