639435e85153d8243ded1fd7ee529255d9be7523
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2025-01-17 18:48:01 +0100

    feat(radio-card): wip

--- packages/radio/src/index.ts
+++ packages/radio/src/index.ts
@@ -1,3 +1,8 @@
 export { default as FzRadio } from "./FzRadio.vue";
 export { default as FzRadioGroup } from "./FzRadioGroup.vue";
-export type { FzRadioProps, FzRadioGroupProps } from "./types";
+export { default as FzRadioCard } from "./FzRadioCard.vue";
+export type {
+  FzRadioProps,
+  FzRadioGroupProps,
+  FzRadioCardProps,
+} from "./types";
