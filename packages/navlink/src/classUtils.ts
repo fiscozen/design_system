aa260ff91c81d7f0cc4e915b5899b45fb389022b
Francesco Panico <francesco.panico@fiscozen.it>
2025-02-19 18:10:04 +0100

    fix(FzNavlink): fix FzRouterNavlink disabled state

--- packages/navlink/src/classUtils.ts
+++ packages/navlink/src/classUtils.ts
@@ -1,2 +1,4 @@
 export const commonClasses =
   'text-grey-500 hover:bg-background-alice-blue border-1 focus:border-1 focus:bg-background-alice-blue disabled:text-grey-100 disabled:bg-core-white h-32 items-center rounded border-transparent text-sm hover:text-blue-500 focus:border-blue-500 focus:text-blue-500 disabled:border-transparent font-medium'
+export const disabledClasses =
+  'text-grey-100 border-1 bg-core-white h-32 items-center rounded border-transparent text-sm border-transparent font-medium cursor-pointer'
