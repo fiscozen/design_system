ac917e0232c6f518863bb8db01617cccfece793b
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2025-07-11 15:44:55 +0200

    Fix/lib 1915 (#223)
    * fix(LIB-1915): add prop to handle icon class
    
    * chore(LIB-1915): bumpo version IconButton
    
    * fix(LIB-1915): change close button color to white
    
    * chore(LIB-1915): bump Toast version
    
    * chore(LIB-1915): fix version bump
    
    * feat(LIB-1915): remove label prop from IconButton

--- packages/button/src/FzIconButton.vue
+++ packages/button/src/FzIconButton.vue
@@ -1,99 +1,92 @@
 <template>
-  <FzButton
-    @click="(e) => $emit('click', e)"
-    :class="classes"
-    :disabled="disabled"
-    :label="label"
-    :size="size"
-    :tooltip="tooltip"
-    :variant="variantMap"
-    overrideContainerClass
-  >
-    <FzIcon :name="iconName" :size="iconSize || mappedIconSize" :variant="iconVariant" />
+  <FzButton @click="(e) => $emit('click', e)" :class="classes" :disabled="disabled" :size="size" :tooltip="tooltip"
+    :variant="variantMap" overrideContainerClass>
+    <FzIcon :name="iconName" :size="iconSize || mappedIconSize" :variant="iconVariant" :class="props.iconClass" />
     <div class="-mr-2 -mt-2" v-if="variant === 'notification'" :class="notificationClasses"></div>
   </FzButton>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-import type { IconButtonVariant, ButtonSize } from './types'
-import { FzIcon, IconSize, IconVariant } from '@fiscozen/icons'
-import FzButton from './FzButton.vue'
-import { iconSizeMap } from './utils'
+  import { computed } from 'vue'
+  import type { IconButtonVariant, ButtonSize } from './types'
+  import { FzIcon, IconSize, IconVariant } from '@fiscozen/icons'
+  import FzButton from './FzButton.vue'
+  import { iconSizeMap } from './utils'
 
-const props = withDefaults(
-  defineProps<{
-    /**
-     * The label of the button
-     */
-    label?: string
-    /**
-     * The tooltip of the button will be shown on hover
-     */
-    tooltip?: string
-    /**
-     * primary or secondary button
-     */
-    variant?: IconButtonVariant
-    /**
-     * size of the button
-     */
-    size?: ButtonSize
-    /**
-     * whether action is enabled or not
-     */
-    disabled?: boolean
-    /**
-     * Icon to be displayed. Use fontawesome search here https://fontawesome.com/v6/icons
-     * Mind that not all of the icons and variants might be available.
-     */
-    iconName: string
-    /**
-     * Fontawesome icon variant: solid, regular, light, thin. Sharp subvariants are available as well
-     */
-    iconVariant?: IconVariant
-    /**
-     * Icon size
-     */
-    iconSize?: IconSize
-    /**
-     * Positioning of the icon
-     */
-    iconPosition?: 'before' | 'after'
-  }>(),
-  {
-    variant: 'primary',
-    size: 'md',
-    disabled: false,
-    iconVariant: 'far',
-    iconPosition: 'before'
-  }
-)
+  const props = withDefaults(
+    defineProps<{
+      /**
+       * The tooltip of the button will be shown on hover
+       */
+      tooltip?: string
+      /**
+       * primary or secondary button
+       */
+      variant?: IconButtonVariant
+      /**
+       * size of the button
+       */
+      size?: ButtonSize
+      /**
+       * whether action is enabled or not
+       */
+      disabled?: boolean
+      /**
+       * Icon to be displayed. Use fontawesome search here https://fontawesome.com/v6/icons
+       * Mind that not all of the icons and variants might be available.
+       */
+      iconName: string
+      /**
+       * Fontawesome icon variant: solid, regular, light, thin. Sharp subvariants are available as well
+       */
+      iconVariant?: IconVariant
+      /**
+       * Icon size
+       */
+      iconSize?: IconSize
+      /**
+       * Positioning of the icon
+       */
+      iconPosition?: 'before' | 'after',
+      /**
+       * Additional class for the icon
+       */
+      iconClass?: string
+    }>(),
+    {
+      variant: 'primary',
+      size: 'md',
+      disabled: false,
+      iconVariant: 'far',
+      iconPosition: 'before',
+      iconClass: ''
+    }
+  )
 
-defineEmits(['click'])
+  defineEmits(['click'])
 
-const variantMap = computed(() => {
-  return props.variant === 'notification' ? 'secondary' : props.variant
-})
+  const variantMap = computed(() => {
+    return props.variant === 'notification' ? 'secondary' : props.variant
+  })
 
-const classes = computed(() => ({
-  'size-24': props.size === 'xs',
-  'w-28 h-28': props.size === 'sm',
-  'w-32 h-32': props.size === 'md',
-  'w-40 h-40': props.size === 'lg'
-}))
+  const classes = computed(() => ({
+    'size-24': props.size === 'xs',
+    'w-28 h-28': props.size === 'sm',
+    'w-32 h-32': props.size === 'md',
+    'w-40 h-40': props.size === 'lg'
+  }))
 
-const notificationClasses = computed(() => ({
-  'rounded-full': true,
-  'bg-blue-500': true,
-  absolute: true,
-  'top-0': true,
-  'right-0': true,
-  'size-4': props.size === 'xs',
-  'w-6 h-6': props.size === 'sm',
-  'w-8 h-8': props.size === 'md' || props.size === 'lg',
-  'bg-grey-200': props.disabled
-}))
+  const notificationClasses = computed(() => ({
+    'rounded-full': true,
+    'bg-blue-500': true,
+    absolute: true,
+    'top-0': true,
+    'right-0': true,
+    'size-4': props.size === 'xs',
+    'w-6 h-6': props.size === 'sm',
+    'w-8 h-8': props.size === 'md' || props.size === 'lg',
+    'bg-grey-200': props.disabled
+  }))
 
-const mappedIconSize = computed(() => iconSizeMap[props.size])
+  const mappedIconSize = computed(() => iconSizeMap[props.size])
 </script>
