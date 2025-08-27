9f462dc7107fc6cbc833f71afb818a5856ad10b9
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2025-08-27 15:04:52 +0200

    feat(LIB-2229): init FzContainer

---
+++ packages/container/src/FzContainer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { FzContainerProps } from "./types";
+
+const props = withDefaults(defineProps<FzContainerProps>(), {});
+
+const emit = defineEmits([]);
+</script>
+
+<style scoped></style>
+
+<template>CONTAINER</template>
