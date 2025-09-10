2a4051b7fbd8321a50da35bebba85111fe13bd07
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2025-09-10 16:52:38 +0200

    feat(LIB-2185): reactive retrive id

--- apps/storybook/src/components/Breweries/Setup.vue
+++ apps/storybook/src/components/Breweries/Setup.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-
-import Retrieve from './Retrieve.vue'
-import All from './All.vue'
-
-// INIT FZ FETCHER
 import { onBeforeMount } from 'vue'
 import { rest } from '@fiscozen/data'
 
@@ -14,13 +9,9 @@ onBeforeMount(() => {
     baseUrl: 'https://api.openbrewerydb.org/v1/'
   })
 })
-// END INIT FZ FETCHER
 </script>
 
 <template>
-  <div class="flex flex-col gap-12">
-    <h1>@fiscozen/data</h1>
-    <Retrieve />
-    <All />
-  </div>
+  <h1>@fiscozen/data</h1>
+  <slot />
 </template>
