6a3e351b6059fc68b435a3eaaf6764c0cb9c4dfc
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2025-09-10 16:19:07 +0200

    feat(LIB-2185): retrieve and all

--- packages/data/src/rest/actions/index.ts
+++ packages/data/src/rest/actions/index.ts
@@ -1,40 +1,41 @@
 import { useFzFetch } from '../dao';
+import { computed } from 'vue';
 
-import type { UseActions } from './types';
+import type { UseFzFetchOptions, UseFzFetchReturn } from '../dao/types';
+
+import type { UseActions, UseActionOptions, UseActionReturn, UseAllAction } from './types';
+
+const normalizeOptions = (options: UseActionOptions = {}): UseFzFetchOptions => ({
+    immediate: options.onMount ?? true,
+    refetch: options.autoUpdate ?? true,
+    initialData: options.initialData ?? null,
+});
+
+const normalizeResponse = <T>(response: UseFzFetchReturn<T>): UseActionReturn<T> => {
+    return {
+        error: computed(() => response.error.value),
+        data: computed(() => response.data.value),
+        isLoading: computed(() => response.isFetching.value),
+        execute: response.execute,
+    } as UseActionReturn<T>;
+};
 
 export const useActions: UseActions = <T>(basePath: string) => {
     return {
-        useRetrieve: (pk, options = {}) => {
+        useRetrieve: (pk, options) => {
+            const response = useFzFetch<T>(`${basePath}/${pk}`, normalizeOptions(options));
 
-            const response = useFzFetch<T>(`${basePath}/${pk}`, {
-                immediate: options.onMount ?? true,
-                refetch: options.autoUpdate ?? false,
-                initialData: options.initialData ?? null,
-            });
-
-            console.log('response', response)
-
-            return {
-                error: response.error,
-                data: response.data,
-                isLoading: response.isFetching,
-                execute: response.execute,
-            };
+            return normalizeResponse<T>(response);
         },
-        useAll: (options = {}) => {
-            const response = useFzFetch<T>(`${basePath}`, {
-                immediate: options.onMount ?? true,
-                refetch: options.autoUpdate ?? false,
-                initialData: options.initialData ?? null,
-            });
-
-            return {
-                error: response.error,
-                data: response.data,
-                isLoading: response.isFetching,
-                execute: response.execute,
-            };
-        },
+        useAll: ((paramsOrOptions, options) => {
+            if (paramsOrOptions !== undefined && options !== undefined) {
+                const response = useFzFetch<T>(`${basePath}`, {/*paramsOrOptions*/}, normalizeOptions(options));
+                return normalizeResponse<T>(response);
+            } else {
+                const response = useFzFetch<T>(`${basePath}`, normalizeOptions(paramsOrOptions as UseActionOptions));
+                return normalizeResponse<T>(response);
+            }
+        }) as UseAllAction<T>,
         /*
         add: () => null,
         modify: () => null,
