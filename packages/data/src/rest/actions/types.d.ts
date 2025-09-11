ecbf784d4cec4c57024af7b1eb7454f68c906f71
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2025-09-11 11:53:56 +0200

    refactor(LIB-2185): rename all in list

--- packages/data/src/rest/actions/types.d.ts
+++ packages/data/src/rest/actions/types.d.ts
@@ -56,14 +56,14 @@ export interface UseRetrieveAction<T> {
 // --------------------------------------------------------
 
 // ALL ACTION
-export interface AllActionParams {
+export interface ListActionParams {
     filters?: Record<string, string | number | boolean>;
     sort?: Record<string, 'asc' | 'desc'>;
     page?: number;
     pageSize?: number;
 }
 
-export interface UseAllActionReturn<T> extends UseActionReturn<T> {
+export interface ListActionReturn<T> extends UseActionReturn<T> {
     /**
      * The retrieved data
      */
@@ -74,17 +74,17 @@ export interface UseAllActionReturn<T> extends UseActionReturn<T> {
     pageSize?: ShallowRef<number>;*/
 }
 
-export interface UseAllAction<T> {
-    (): UseAllActionReturn<T>;
-    (paramsOrOptions: AllActionParams | UseActionOptions): UseAllActionReturn<T>;
-    (paramsOrOptions: AllActionParams, options: UseActionOptions): UseAllActionReturn<T>;
+export interface UseListAction<T> {
+    (): ListActionReturn<T>;
+    (paramsOrOptions: ListActionParams | UseActionOptions): ListActionReturn<T>;
+    (paramsOrOptions: ListActionParams, options: UseActionOptions): ListActionReturn<T>;
 }
 // --------------------------------------------------------
 
 // USE ACTIONS
 interface UseActionsReturn<T> {
     useRetrieve: UseRetrieveAction<T>;
-    useAll: UseAllAction<T>;
+    useList: UseListAction<T>;
 }
 
 export interface UseActions {
