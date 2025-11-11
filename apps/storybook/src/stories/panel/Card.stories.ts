e2aa3a67be28d3483ef58690743e67e57820fd12
Riccardo Agnoletto <riccardo.agnoletto@fiscozen.it>
2025-11-11 17:02:41 +0100

    LIB-1889 feat(card): update to 1.0.0 design

--- apps/storybook/src/stories/panel/Card.stories.ts
+++ apps/storybook/src/stories/panel/Card.stories.ts
@@ -10,6 +10,29 @@ const meta = {
     // it will not be generated in the typescript definition probabily because it has '- ' in the name
     'header-content': {
       description: 'Slot for the header content, it will be displayed below the title'
+    },
+    color: {
+      control: 'select',
+      options: ['default', 'blue', 'orange', 'purple', 'grey', 'aliceblue'],
+      description: 'Card background color variant. Note: "aliceblue" is deprecated, use "blue" instead.',
+      table: {
+        defaultValue: { summary: 'default' }
+      }
+    },
+    hasInfoIcon: {
+      control: 'boolean',
+      description: 'Whether to show an info icon in the header',
+      table: {
+        defaultValue: { summary: 'false' }
+      }
+    },
+    environment: {
+      control: 'select',
+      options: ['backoffice', 'frontoffice'],
+      description: 'The environment context for the card buttons',
+      table: {
+        defaultValue: { summary: 'frontoffice' }
+      }
     }
   }
 } satisfies Meta<typeof FzCard>
@@ -29,13 +52,18 @@ const Template: CardStory = {
     methods: {
       onPrimaryAction: () => console.log('Primary action clicked'),
       onSecondaryAction: () => console.log('Secondary action clicked'),
-      onTertiaryAction: () => console.log('Tertiary action clicked')
+      onTertiaryAction: () => console.log('Tertiary action clicked'),
+      onInfoClick: () => console.log('Info icon clicked')
     },
-    template: `<FzCard v-bind="args" class="w-[500px] m-8" @fzprimary:click="onPrimaryAction" @fzsecondary:click="onSecondaryAction" @fztertiary:click="onTertiaryAction"> 
+    template: `<FzCard v-bind="args" class="w-[500px] m-8" @fzprimary:click="onPrimaryAction" @fzsecondary:click="onSecondaryAction" @fztertiary:click="onTertiaryAction" @fzcard:click-info="onInfoClick"> 
                         <template #header>
                             <FzBadge color="warning"> Attesa Utente </FzBadge>
                         </template>
-                        <div> Content </div>
+                        <div class="border-2 border-dashed border-grey-200 rounded p-16 flex items-center justify-center text-center h-full w-full">
+                          <p class="text-grey-500 m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+                          </p>
+                        </div>
                     </FzCard>`
   }),
   args: {
@@ -67,6 +95,29 @@ export const CardWithActions: CardStory = {
   }
 }
 
+export const CardWithSecondaryActionOnly: CardStory = {
+  ...Template,
+  args: {
+    ...Template.args,
+    secondaryAction: {
+      label: 'Secondary Action'
+    }
+  }
+}
+
+export const CardWithPrimaryAndSecondary: CardStory = {
+  ...Template,
+  args: {
+    ...Template.args,
+    primaryAction: {
+      label: 'Primary'
+    },
+    secondaryAction: {
+      label: 'Secondary'
+    }
+  }
+}
+
 export const CardPurple: CardStory = {
   ...CardWithActions,
   args: {
@@ -83,6 +134,14 @@ export const CardOrange: CardStory = {
   }
 }
 
+export const CardDefault: CardStory = {
+  ...CardWithActions,
+  args: {
+    ...CardWithActions.args,
+    color: 'default'
+  }
+}
+
 export const CardBlue: CardStory = {
   ...CardWithActions,
   args: {
@@ -91,11 +150,44 @@ export const CardBlue: CardStory = {
   }
 }
 
+export const CardGrey: CardStory = {
+  ...CardWithActions,
+  args: {
+    ...CardWithActions.args,
+    color: 'grey'
+  }
+}
+
+export const CardWithInfoIcon: CardStory = {
+  ...CardWithActions,
+  args: {
+    ...CardWithActions.args,
+    hasInfoIcon: true
+  }
+}
+
+export const CardBackoffice: CardStory = {
+  ...CardWithActions,
+  args: {
+    ...CardWithActions.args,
+    environment: 'backoffice'
+  }
+}
+
+export const CardFrontoffice: CardStory = {
+  ...CardWithActions,
+  args: {
+    ...CardWithActions.args,
+    environment: 'frontoffice'
+  }
+}
+
 export const CardCollapsible: CardStory = {
   ...CardWithActions,
   args: {
     ...CardWithActions.args,
-    collapsible: true
+    collapsible: true,
+    hasInfoIcon: true
   }
 }
 
