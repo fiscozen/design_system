70c126e7891254820498ca57a171c5628520b59e
Francesca Vago <francesca.vago@fiscozen.it>
2026-03-10 14:49:37 +0100

    refactor(LIB-1920): enhance FzViewFlag component layout and add banner slot

--- packages/view-flag/src/FzViewFlag.vue
+++ packages/view-flag/src/FzViewFlag.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="border-8 border-semantic-warning fixed inset-0 pointer-events-none z-50">
     <div
-    class="bg-semantic-warning rounded-t-base text-sm p-8 bottom-0 left-0 right-0 fixed m-auto empty:hidden w-fit pointer-events-auto"
+    class="bg-semantic-warning rounded-t-base text-sm p-8 bottom-0 left-0 right-0 fixed m-auto empty:hidden w-full sm:w-fit pointer-events-auto"
     >
     <slot />
+    <div v-if="open" class="w-full relative">
+      <div class="bg-semantic-warning w-dvw -translate-x-1/2 left-1/2 top-0 bottom-0 absolute"></div>
+      <div class="isolate">
+        <slot name="banner"/>
+      </div>
+    </div>
   </div>
 </div>
 </template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+  open?: boolean
+}>()
+
+</script>
