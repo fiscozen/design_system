b0346afc36b19edc3d14a9d3a17c8edea7500747
Sergio Terrasi <sergio.terrasi@fiscozen.it>
2026-06-25 17:06:42 +0200

    fix(radio): self-contained label spacing (mb-0 baseline)

--- packages/radio/src/common.ts
+++ packages/radio/src/common.ts
@@ -3,6 +3,6 @@ export const beforeLabelClass =  "before:h-16 before:w-16 peer-checked:before:bo
 export const staticInputClass = "peer h-0 w-0 absolute fz-hidden-input";
 export const staticLabelClass = `
   flex items-start gap-4 
-  text-core-black
+  text-core-black mb-0
   fz-radio__label
 `;
